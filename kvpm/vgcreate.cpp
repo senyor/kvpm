2e8f2bdc25a2dcf250d40fe4d2eafbf67a4a84e9
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2008-02-28 04:23:16 +0000

    VGCreate gets a name validator!

--- kvpm/vgcreate.cpp
+++ kvpm/vgcreate.cpp
@@ -14,27 +14,28 @@
 
 
 #include <QtGui>
+
 #include "processprogress.h"
 #include "vgcreate.h"
 
 
-bool create_vg(QString PhysicalVolumePath)
+bool create_vg(QString physicalVolumePath)
 {
-    VGCreateDialog dialog(PhysicalVolumePath);
+    VGCreateDialog dialog(physicalVolumePath);
     dialog.exec();
     
     if(dialog.result() == QDialog::Accepted){
         ProcessProgress create_vg( dialog.arguments(), "Creating vg...", true );
-        return TRUE;
+        return true;
     }
     else
-        return FALSE;
+        return false;
 }
 
 
-VGCreateDialog::VGCreateDialog(QString PhysicalVolumePath, QWidget *parent) : 
+VGCreateDialog::VGCreateDialog(QString physicalVolumePath, QWidget *parent) : 
     KDialog(parent),
-    pv_path(PhysicalVolumePath)
+    m_pv_path(physicalVolumePath)
 {
 
     setWindowTitle(tr("Create Volume Group"));
@@ -45,86 +46,95 @@ VGCreateDialog::VGCreateDialog(QString PhysicalVolumePath, QWidget *parent) :
     dialog_body->setLayout(layout);
 
     QLabel *name_label = new QLabel("Volume Group Name: ");
-    vg_name = new KLineEdit();
+    m_vg_name = new KLineEdit();
+
+    QRegExp rx("[0-9a-zA-Z_\\.][-0-9a-zA-Z_\\.]*");
+    m_validator = new QRegExpValidator( rx, m_vg_name );
+    m_vg_name->setValidator(m_validator);
     QHBoxLayout *name_layout = new QHBoxLayout();
     name_layout->addWidget(name_label);
-    name_layout->addWidget(vg_name);
+    name_layout->addWidget(m_vg_name);
 
     QLabel *extent_label = new QLabel("Physical Extent Size: ");
-    extent_size = new KComboBox();
-    extent_size->insertItem(0,"1");
-    extent_size->insertItem(1,"2");
-    extent_size->insertItem(2,"4");
-    extent_size->insertItem(3,"8");
-    extent_size->insertItem(4,"16");
-    extent_size->insertItem(5,"32");
-    extent_size->insertItem(6,"64");
-    extent_size->insertItem(7,"128");
-    extent_size->insertItem(8,"256");
-    extent_size->insertItem(9,"512");
-    extent_size->setInsertPolicy(QComboBox::NoInsert);
-    extent_size->setCurrentIndex(2);
-    extent_suffix = new KComboBox();
-    extent_suffix->insertItem(0,"KB");
-    extent_suffix->insertItem(1,"MB");
-    extent_suffix->insertItem(2,"GB");
-    extent_suffix->setInsertPolicy(QComboBox::NoInsert);
-    extent_suffix->setCurrentIndex(1);
+    m_extent_size = new KComboBox();
+    m_extent_size->insertItem(0,"1");
+    m_extent_size->insertItem(1,"2");
+    m_extent_size->insertItem(2,"4");
+    m_extent_size->insertItem(3,"8");
+    m_extent_size->insertItem(4,"16");
+    m_extent_size->insertItem(5,"32");
+    m_extent_size->insertItem(6,"64");
+    m_extent_size->insertItem(7,"128");
+    m_extent_size->insertItem(8,"256");
+    m_extent_size->insertItem(9,"512");
+    m_extent_size->setInsertPolicy(QComboBox::NoInsert);
+    m_extent_size->setCurrentIndex(2);
+    m_extent_suffix = new KComboBox();
+    m_extent_suffix->insertItem(0,"KB");
+    m_extent_suffix->insertItem(1,"MB");
+    m_extent_suffix->insertItem(2,"GB");
+    m_extent_suffix->setInsertPolicy(QComboBox::NoInsert);
+    m_extent_suffix->setCurrentIndex(1);
     
     QHBoxLayout *extent_layout = new QHBoxLayout();
     extent_layout->addWidget(extent_label);
-    extent_layout->addWidget(extent_size);
-    extent_layout->addWidget(extent_suffix);
+    extent_layout->addWidget(m_extent_size);
+    extent_layout->addWidget(m_extent_suffix);
 
     QGroupBox *lv_box = new QGroupBox("Number of Logical Volumes");
     QVBoxLayout *lv_layout_v = new QVBoxLayout();
     QHBoxLayout *lv_layout_h = new QHBoxLayout();
     lv_box->setLayout(lv_layout_v);
-    max_lvs_check = new QCheckBox("No Limit");
-    max_lvs_check->setCheckState(Qt::Checked);
-    lv_layout_v->addWidget(max_lvs_check);
+    m_max_lvs_check = new QCheckBox("No Limit");
+    m_max_lvs_check->setCheckState(Qt::Checked);
+    lv_layout_v->addWidget(m_max_lvs_check);
     lv_layout_v->addLayout(lv_layout_h);
     QLabel *lv_label = new QLabel("Maximum: ");
-    max_lvs = new KLineEdit();
+    m_max_lvs = new KLineEdit();
     QIntValidator *lv_validator = new QIntValidator(1,255,this);
-    max_lvs->setValidator(lv_validator);
-    max_lvs->setEnabled(FALSE);
+    m_max_lvs->setValidator(lv_validator);
+    m_max_lvs->setEnabled(false);
     lv_layout_h->addWidget(lv_label);
-    lv_layout_h->addWidget(max_lvs);
+    lv_layout_h->addWidget(m_max_lvs);
 
     QGroupBox *pv_box = new QGroupBox("Number of Physical Volumes");
     QVBoxLayout *pv_layout_v = new QVBoxLayout();
     QHBoxLayout *pv_layout_h = new QHBoxLayout();
     pv_box->setLayout(pv_layout_v);
-    max_pvs_check = new QCheckBox("No Limit");
-    max_pvs_check->setCheckState(Qt::Checked);
-    pv_layout_v->addWidget(max_pvs_check);
+    m_max_pvs_check = new QCheckBox("No Limit");
+    m_max_pvs_check->setCheckState(Qt::Checked);
+    pv_layout_v->addWidget(m_max_pvs_check);
     pv_layout_v->addLayout(pv_layout_h);
     QLabel *pv_label = new QLabel("Maximum: ");
-    max_pvs = new KLineEdit();
+    m_max_pvs = new KLineEdit();
     QIntValidator *pv_validator = new QIntValidator(1,255,this);
-    max_pvs->setValidator(pv_validator);
-    max_pvs->setEnabled(FALSE);
+    m_max_pvs->setValidator(pv_validator);
+    m_max_pvs->setEnabled(false);
     pv_layout_h->addWidget(pv_label);
-    pv_layout_h->addWidget(max_pvs);
+    pv_layout_h->addWidget(m_max_pvs);
 
-    clustered = new QCheckBox("Cluster Aware");
-    clustered->setEnabled(FALSE);
+    m_clustered = new QCheckBox("Cluster Aware");
+    m_clustered->setEnabled(false);
     
-    auto_backup = new QCheckBox("Automatic Backup");
-    auto_backup->setCheckState(Qt::Checked);
+    m_auto_backup = new QCheckBox("Automatic Backup");
+    m_auto_backup->setCheckState(Qt::Checked);
 
     layout->addLayout(name_layout);
     layout->addLayout(extent_layout);
     layout->addWidget(lv_box);
     layout->addWidget(pv_box);
-    layout->addWidget(clustered);
-    layout->addWidget(auto_backup);
+    layout->addWidget(m_clustered);
+    layout->addWidget(m_auto_backup);
 
-    connect(max_lvs_check, SIGNAL(stateChanged(int)), 
+    enableButtonOk(false);
+
+    connect(m_vg_name, SIGNAL(textChanged(QString)), 
+	    this, SLOT(validateName(QString)));
+
+    connect(m_max_lvs_check, SIGNAL(stateChanged(int)), 
 	    this, SLOT(limitLogicalVolumes(int)));
 
-    connect(max_pvs_check, SIGNAL(stateChanged(int)), 
+    connect(m_max_pvs_check, SIGNAL(stateChanged(int)), 
 	    this, SLOT(limitPhysicalVolumes(int)));
 }
 
@@ -134,25 +144,25 @@ QStringList VGCreateDialog::arguments()
     
     args << "/sbin/vgcreate"
 	 << "--physicalextentsize"
-	 << extent_size->currentText() + extent_suffix->currentText().at(0);
+	 << m_extent_size->currentText() + m_extent_suffix->currentText().at(0);
     
-    if(clustered->isChecked())
+    if(m_clustered->isChecked())
 	args << "--clustered" << "y";
     else
 	args << "--clustered" << "n";
 
-    if(auto_backup->isChecked())
+    if(m_auto_backup->isChecked())
 	args << "--autobackup" << "y" ;
     else
 	args << "--autobackup" << "n" ;
 
-    if((!max_lvs_check->isChecked()) && (max_lvs->text() != ""))
-	args << "--maxlogicalvolumes" << max_lvs->text();
+    if((!m_max_lvs_check->isChecked()) && (m_max_lvs->text() != ""))
+	args << "--maxlogicalvolumes" << m_max_lvs->text();
 
-    if((!max_pvs_check->isChecked()) && (max_pvs->text() != ""))
-	args << "--maxphysicalvolumes" << max_pvs->text();
+    if((!m_max_pvs_check->isChecked()) && (m_max_pvs->text() != ""))
+	args << "--maxphysicalvolumes" << m_max_pvs->text();
     
-    args << vg_name->text() << pv_path;
+    args << m_vg_name->text() << m_pv_path;
 
     return args;
 }
@@ -160,15 +170,33 @@ QStringList VGCreateDialog::arguments()
 void VGCreateDialog::limitLogicalVolumes(int boxstate)
 {
     if(boxstate == Qt::Unchecked)
-	max_lvs->setEnabled(TRUE);
+	m_max_lvs->setEnabled(true);
     else
-	max_lvs->setEnabled(FALSE);
+	m_max_lvs->setEnabled(false);
 }
 
 void VGCreateDialog::limitPhysicalVolumes(int boxstate)
 {
     if(boxstate == Qt::Unchecked)
-	max_pvs->setEnabled(TRUE);
+	m_max_pvs->setEnabled(true);
     else
-	max_pvs->setEnabled(FALSE);
+	m_max_pvs->setEnabled(false);
+}
+
+/* The allowed characters in the name are letters, numbers, periods
+   hyphens and underscores. Also the names ".", ".." and names starting
+   with a hyphen are disallowed */
+
+void VGCreateDialog::validateName(QString name)
+{
+    int pos = 0;
+
+    if( m_validator->validate(name, pos) == QValidator::Acceptable &&
+	name != "." && 
+	name != ".." )
+    {
+	enableButtonOk(true);
+    }
+    else
+	enableButtonOk(false);
 }
--- kvpm/vgcreate.h
+++ kvpm/vgcreate.h
@@ -11,34 +11,46 @@
  * 
  * See the file "COPYING" for the exact licensing terms.
  */
+
 #ifndef VGCREATE_H
 #define VGCREATE_H
 
 #include <KDialog>
 #include <KLineEdit>
 #include <KComboBox>
+
 #include <QStringList>
 #include <QCheckBox>
+#include <QRegExpValidator>
 
-bool create_vg(QString PhysicalVolumePath);
+
+bool create_vg(QString physicalVolumePath);
 
 
 class VGCreateDialog : public KDialog
 {
 Q_OBJECT
-    QString pv_path;
-    KLineEdit *vg_name, *max_lvs, *max_pvs;
-    QCheckBox *clustered, *auto_backup, *max_lvs_check, *max_pvs_check;
-    KComboBox *extent_size, *extent_suffix;
+
+    QString   m_pv_path;
+
+    KLineEdit *m_vg_name, *m_max_lvs, *m_max_pvs;
+
+    QCheckBox *m_clustered, *m_auto_backup, 
+	      *m_max_lvs_check, *m_max_pvs_check;
+
+    KComboBox *m_extent_size, *m_extent_suffix;
+
+    QRegExpValidator *m_validator;
     
  public:
-    VGCreateDialog(QString PhysicalVolumePath, QWidget *parent = 0);
+    VGCreateDialog(QString physicalVolumePath, QWidget *parent = 0);
     QStringList arguments();
     
  private slots:
     void limitLogicalVolumes(int boxstate);
     void limitPhysicalVolumes(int boxstate);
- 
+    void validateName(QString name);
+    
 };
 
 #endif
