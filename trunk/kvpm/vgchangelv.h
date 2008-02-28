7508fa8263339385e297599e03ed33c5676c90ef
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2008-02-28 04:58:02 +0000

    vgchangelv and vgchangealloc get the style update this time

--- trunk/kvpm/vgchangelv.h
+++ trunk/kvpm/vgchangelv.h
@@ -11,28 +11,30 @@
  * 
  * See the file "COPYING" for the exact licensing terms.
  */
+
 #ifndef VGCHANGELV_H
 #define VGCHANGELV_H
 
 #include <KDialog>
+
 #include <QStringList>
 #include <QSpinBox>
 #include <QGroupBox>
 
 class VolGroup;
 
-bool change_vg_lv(VolGroup *VolumeGroup);
+bool change_vg_lv(VolGroup *volumeGroup);
 
 
 class VGChangeLVDialog : public KDialog
 {
-    QString vg_name;
-    VolGroup *vg;
-    QGroupBox *limit_lvs;
-    QSpinBox *max_lvs;
+    QString    m_vg_name;
+    VolGroup  *m_vg;
+    QGroupBox *m_limit_lvs;
+    QSpinBox  *m_max_lvs;
     
 public:
-    VGChangeLVDialog(VolGroup *VolumeGroup, QWidget *parent = 0);
+    VGChangeLVDialog(VolGroup *volumeGroup, QWidget *parent = 0);
     QStringList arguments();
 };
 
