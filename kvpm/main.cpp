7277470cb9c38e68634113d2b6755a1d631d8c6a
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2008-03-04 03:31:21 +0000

    Bumped up the version number.

--- kvpm/main.cpp
+++ kvpm/main.cpp
@@ -36,27 +36,26 @@ ExecutableFinder *g_executable_finder;
 
 int main(int argc, char **argv)
 {
-
+    
     KAboutData aboutData( "kvpm", 0,
-			  ki18n("kvpm"), "0.3.3",
+			  ki18n("kvpm"), "0.4.0",
 			  ki18n("Linux volume and partition manager for KDE"),
 			  KAboutData::License_GPL,
 			  ki18n("Copyright (c) 2008 Benjamin Scott") );
 
     KCmdLineArgs::init( argc, argv, &aboutData);
+
     KApplication kvpm;
 
-    if( geteuid() ){
-	if( seteuid(0) ){
-	    KMessageBox::error( 0, 
-				"This program must be run as root (uid = 0)",
-				"Insufficient Privilege");
-	    return 0;
-	}
+    if( geteuid() != 0 ){
+	
+	KMessageBox::error( 0, 
+			    "This program must be run as root (uid = 0)",
+			    "Insufficient Privilege");
+	return 0;
     }
 
     g_executable_finder = new ExecutableFinder();
-    
     
     MainWindow = new TopWindow(NULL);
     MainWindow->setGeometry(0,0,1000,750);
