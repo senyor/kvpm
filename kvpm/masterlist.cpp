eaf82ea76375dd80f1cf330cd13a468fabf01113
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2008-03-17 00:15:11 +0000

    Jus more coding style improvements

--- kvpm/masterlist.cpp
+++ kvpm/masterlist.cpp
@@ -28,10 +28,10 @@
 
 
 MasterList::MasterList() : QObject()
-{ 
-    scanVolGroups();
-    scanLogVols();
-    scanPhysVols();
+{
+    scanVolumeGroups();
+    scanLogicalVolumes();
+    scanPhysicalVolumes();
     scanStorageDevices();
 }
 
@@ -42,22 +42,22 @@ MasterList::MasterList() : QObject()
 
 MasterList::~MasterList()
 {
-    for(int x = 0; x < VolGroups.size(); x++)
-	delete VolGroups[x];
+    for(int x = 0; x < m_volume_groups.size(); x++)
+	delete m_volume_groups[x];
 
-    for(int x = 0; x < LogVols.size(); x++)
-	delete LogVols[x];
+    for(int x = 0; x < m_logical_volumes.size(); x++)
+	delete m_logical_volumes[x];
 
-    for(int x = 0; x < PhysVols.size(); x++)
-	delete PhysVols[x];
+    for(int x = 0; x < m_physical_volumes.size(); x++)
+	delete m_physical_volumes[x];
 
-    for(int x = 0; x < StorageDevices.size(); x++)
-	delete StorageDevices[x];
+    for(int x = 0; x < m_storage_devices.size(); x++)
+	delete m_storage_devices[x];
 }
 
 /* The next function takes a VolGroup object as its parameter
    then rescans the information for the volume group it represents.
-   The resulting VolGroup object is placed in the VolGroups
+   The resulting VolGroup object is placed in the m_volume_groups
    list at the location of the original, which is then deleted. 
    This function is only useful when the operation, such a changing
    logical volume attributes, is one that won't effect other groups 
@@ -69,22 +69,22 @@ VolGroup* MasterList::rebuildVolumeGroup(VolGroup *volumeGroup)
     VolGroup *new_group, *old_group;
 
     old_group = volumeGroup;
-    new_group = scanVolGroups( volumeGroup->getName() );
+    new_group = scanVolumeGroups( old_group->getName() );
 
-    for(int x = 0; x <  VolGroups.size(); x++){
-	if(VolGroups[x] == old_group)
-	    VolGroups[x] = new_group;
+    for(int x = 0; x <  m_volume_groups.size(); x++){
+	if(m_volume_groups[x] == old_group)
+	    m_volume_groups[x] = new_group;
     }
 
     delete old_group;
 
-    scanLogVols(new_group);
-    scanPhysVols(new_group);
+    scanLogicalVolumes(new_group);
+    scanPhysicalVolumes(new_group);
 
     return new_group;
 }
 
-void MasterList::scanVolGroups()
+void MasterList::scanVolumeGroups()
 {
     QStringList vg_output;
     QStringList arguments;
@@ -95,18 +95,20 @@ void MasterList::scanVolGroups()
     
     arguments << "vgs" << "-o" 
 	      <<  "+vg_extent_size,vg_extent_count,vg_free_count,vg_fmt,max_pv,max_lv"  
-	      << "--noheadings" << "--separator" << "|" << "--nosuffix" 
-	      << "--units" << "b" << "--partial"; 
+	      << "--noheadings" << "--separator" 
+	      << "|" << "--nosuffix" 
+	      << "--units" << "b" 
+	      << "--partial"; 
     
     ProcessProgress vgscan(arguments, "Scanning volume groups");
     vg_output = vgscan.programOutput();
     vg_count = vg_output.size();
     
     for(int x = 0 ; x < vg_count ; x++)
-	VolGroups.append(new VolGroup(vg_output[x]));
+	m_volume_groups.append(new VolGroup(vg_output[x]));
 }
 
-VolGroup* MasterList::scanVolGroups(QString VolumeGroupName)
+VolGroup* MasterList::scanVolumeGroups(QString VolumeGroupName)
 {
     QStringList vg_output;
     QStringList arguments;
@@ -116,8 +118,10 @@ VolGroup* MasterList::scanVolGroups(QString VolumeGroupName)
     
     arguments << "vgs" << "-o" 
 	      <<  "+vg_extent_size,vg_extent_count,vg_free_count,vg_fmt,max_pv,max_lv"  
-	      << "--noheadings" << "--separator" << "|" << "--nosuffix" 
-	      << "--units" << "b" << "--partial" << VolumeGroupName; 
+	      << "--noheadings" << "--separator" 
+	      << "|" << "--nosuffix" 
+	      << "--units" << "b" 
+	      << "--partial" << VolumeGroupName; 
     
     ProcessProgress vgscan(arguments, "Scanning volume groups");
     vg_output = vgscan.programOutput();
@@ -125,7 +129,7 @@ VolGroup* MasterList::scanVolGroups(QString VolumeGroupName)
     return (new VolGroup(vg_output[0]));
 }
 
-void MasterList::scanLogVols()
+void MasterList::scanLogicalVolumes()
 {
     QStringList lv_output;
     QStringList seg_data;
@@ -139,8 +143,10 @@ void MasterList::scanLogVols()
     
     arguments << "lvs" << "--all" << "-o" 
 	      << "lv_name,vg_name,lv_attr,lv_size,origin,snap_percent,move_pv,mirror_log,copy_percent,chunksize,seg_count,stripes,stripesize,seg_size,devices,lv_kernel_major,lv_kernel_minor,lv_minor"  
-	      << "--noheadings" << "--separator" << "|" << "--nosuffix" 
-	      << "--units" << "b" << "--partial"; 
+	      << "--noheadings" << "--separator" 
+	      << "|" << "--nosuffix" 
+	      << "--units" << "b" 
+	      << "--partial"; 
     
     ProcessProgress lvscan(arguments, "Scanning logical volumes...");
     lv_output = lvscan.programOutput();
@@ -153,25 +159,25 @@ void MasterList::scanLogVols()
 	    seg_data << lv_output[i++];
 	}
 
-	LogVols.append(new LogVol(seg_data, &mount_info_list));
+	m_logical_volumes.append(new LogVol(seg_data, &mount_info_list));
     } 
     
     /* put pointers to the lvs in their associated vgs */
     
-    lv_count = LogVols.size();
-    vg_count = VolGroups.size();
+    lv_count = m_logical_volumes.size();
+    vg_count = m_volume_groups.size();
     
     for(int vg = 0; vg < vg_count; vg++){
 	for(int lv = 0; lv < lv_count; lv++){
-	    if(LogVols[lv]->getVolumeGroupName() == VolGroups[vg]->getName()){
-		VolGroups[vg]->addLogicalVolume(LogVols[lv]);
-		LogVols[lv]->setVolumeGroup(VolGroups[vg]);
+	    if(m_logical_volumes[lv]->getVolumeGroupName() == m_volume_groups[vg]->getName()){
+		m_volume_groups[vg]->addLogicalVolume(m_logical_volumes[lv]);
+		m_logical_volumes[lv]->setVolumeGroup(m_volume_groups[vg]);
 	    }
 	}    
     }
 }
 
-void MasterList::scanLogVols(VolGroup *VolumeGroup)
+void MasterList::scanLogicalVolumes(VolGroup *VolumeGroup)
 {
     LogVol *logical_volume;
     QString group_name;
@@ -186,8 +192,10 @@ void MasterList::scanLogVols(VolGroup *VolumeGroup)
 
     arguments << "lvs" << "--all" << "-o" 
 	      << "lv_name,vg_name,lv_attr,lv_size,origin,snap_percent,move_pv,mirror_log,copy_percent,chunksize,seg_count,stripes,stripesize,seg_size,devices,lv_kernel_major,lv_kernel_minor,lv_minor"  
-	      << "--noheadings" << "--separator" << "|" << "--nosuffix" 
-	      << "--units" << "b" << "--partial" << group_name; 
+	      << "--noheadings" << "--separator" 
+	      << "|" << "--nosuffix" 
+	      << "--units" << "b" 
+	      << "--partial" << group_name; 
     
     ProcessProgress lvscan(arguments, "Scanning logical volumes...");
     lv_output = lvscan.programOutput();
@@ -211,39 +219,45 @@ void MasterList::scanLogVols(VolGroup *VolumeGroup)
 /*  Once the second program 'lvs' completes we start up
     the last one 'pvs' and then get its output    */
 
-void MasterList::scanPhysVols()
+void MasterList::scanPhysicalVolumes()
 {
     QStringList pv_output;
     QStringList arguments;
     int pv_count;
     int vg_count = getVolGroupCount();
     
-    arguments << "pvs" << "--noheadings" << "--separator" << "|" << "--nosuffix" 
-	      << "--units" << "b" << "--partial" << "-o" << "+pv_used,pv_uuid";
+    arguments << "pvs" 
+	      << "--noheadings" 
+	      << "--separator" << "|" 
+	      << "--nosuffix" 
+	      << "--units" << "b" 
+	      << "--partial" 
+	      << "-o" << "+pv_used,pv_uuid";
     
     ProcessProgress pvscan(arguments, "Scanning physical volumes...");
     pv_output = pvscan.programOutput();
     
     pv_count = pv_output.size();
 
-    for(int x = 0; x < PhysVols.size(); x++) // Delete everything in the old list
-	delete PhysVols[x];
-    PhysVols.clear();
+    for(int x = 0; x < m_physical_volumes.size(); x++) // Delete everything in the old list
+	delete m_physical_volumes[x];
+
+    m_physical_volumes.clear();
     
     for(int x = 0; x < pv_count; x++)
-	PhysVols.append(new PhysVol(pv_output[x]));
+	m_physical_volumes.append(new PhysVol(pv_output[x]));
 
     for(int vg = 0; vg < vg_count; vg++){
-	VolGroups[vg]->clearPhysicalVolumes();
+	m_volume_groups[vg]->clearPhysicalVolumes();
 	for(int pv = 0; pv < pv_count; pv++){
-	    if(PhysVols[pv]->getVolumeGroupName() == VolGroups[vg]->getName()){
-		VolGroups[vg]->addPhysicalVolume(PhysVols[pv]);
+	    if(m_physical_volumes[pv]->getVolumeGroupName() == m_volume_groups[vg]->getName()){
+		m_volume_groups[vg]->addPhysicalVolume(m_physical_volumes[pv]);
 	    }
 	}
     }
 }
 
-void MasterList::scanPhysVols(VolGroup *VolumeGroup)
+void MasterList::scanPhysicalVolumes(VolGroup *VolumeGroup)
 {
     PhysVol *physical_volume;
     QString group_name;
@@ -253,25 +267,31 @@ void MasterList::scanPhysVols(VolGroup *VolumeGroup)
 
     group_name = VolumeGroup->getName();
     
-    arguments << "pvs" << "--noheadings" << "--separator" << "|" << "--nosuffix" 
-	      << "--units" << "b" << "--partial" << "-o" << "+pv_used,pv_uuid";
+    arguments << "pvs" 
+	      << "--noheadings" 
+	      << "--separator" << "|" 
+	      << "--nosuffix" 
+	      << "--units" << "b" 
+	      << "--partial" 
+	      << "-o" << "+pv_used,pv_uuid";
     
     ProcessProgress pvscan(arguments, "Scanning physical volumes...");
     pv_output = pvscan.programOutput();
     
     pv_count = pv_output.size();
 
-    for(int x = PhysVols.size() - 1; x >= 0; x--) // Delete old list entries for this group
-	if(PhysVols[x]->getVolumeGroupName() == group_name){
-	    delete PhysVols[x];
-	    PhysVols.removeAt(x);
+    for(int x = m_physical_volumes.size() - 1; x >= 0; x--) // Delete old list entries for this group
+	if(m_physical_volumes[x]->getVolumeGroupName() == group_name){
+	    delete m_physical_volumes[x];
+	    m_physical_volumes.removeAt(x);
 	}
     
     for(int x = 0; x < pv_count; x++){
 	physical_volume = new PhysVol(pv_output[x]);
+
 	if(physical_volume->getVolumeGroupName() == group_name){
 	    VolumeGroup->addPhysicalVolume(physical_volume);
-	    PhysVols.append(physical_volume);
+	    m_physical_volumes.append(physical_volume);
 	}
 	else
 	    delete physical_volume;
@@ -282,68 +302,75 @@ void MasterList::scanStorageDevices()
 {
     PedDevice *dev = NULL;
     
-    for(int x = 0; x < StorageDevices.size(); x++)
-	delete StorageDevices[x];
-    StorageDevices.clear();
+    for(int x = 0; x < m_storage_devices.size(); x++)
+	delete m_storage_devices[x];
+
+    m_storage_devices.clear();
 
     ped_device_probe_all();
 
     MountInformationList *mount_info_list = new MountInformationList();
 
     while( ( dev = ped_device_get_next(dev) ) ){
-	StorageDevices.append( new StorageDevice(dev, PhysVols, mount_info_list ) );
+	m_storage_devices.append( new StorageDevice(dev, m_physical_volumes, mount_info_list ) );
     }
 }
 
 int MasterList::getVolGroupCount()
 {
-      return VolGroups.size();
+      return m_volume_groups.size();
 }
 
 int MasterList::getPhysVolCount()
 {
-      return PhysVols.size();
+      return m_physical_volumes.size();
 }
 
 const QList<VolGroup *> MasterList::getVolGroups()
 {
-      return VolGroups;
+      return m_volume_groups;
 }
 
 const QList<PhysVol *> MasterList::getPhysVols()
 {
-    return PhysVols;
+    return m_physical_volumes;
 }
 
 const QList<StorageDevice *> MasterList::getStorageDevices()
 {
-    return StorageDevices;
+    return m_storage_devices;
 }
 
 PhysVol* MasterList::getPhysVolByName(QString name)
 {
     name = name.trimmed();
-    for(int x = 0; x < PhysVols.size(); x++){
-	if(name == PhysVols[x]->getDeviceName())
-	    return PhysVols[x];
+
+    for(int x = 0; x < m_physical_volumes.size(); x++){
+	if(name == m_physical_volumes[x]->getDeviceName())
+	    return m_physical_volumes[x];
     }
+
     return NULL;
 }
 
 VolGroup* MasterList::getVolGroupByName(QString name)
 {
     name = name.trimmed();
-    for(int x = 0; x < VolGroups.size(); x++){
-	if(name == VolGroups[x]->getName())
-	    return VolGroups[x];
+
+    for(int x = 0; x < m_volume_groups.size(); x++){
+	if(name == m_volume_groups[x]->getName())
+	    return m_volume_groups[x];
     }
+
     return NULL;
 }
 
 QStringList MasterList::getVolumeGroupNames()
 {
     QStringList names;
-    for(int x = 0; x < VolGroups.size(); x++)
-	names << VolGroups[x]->getName();
+
+    for(int x = 0; x < m_volume_groups.size(); x++)
+	names << m_volume_groups[x]->getName();
+
     return names;
 }
