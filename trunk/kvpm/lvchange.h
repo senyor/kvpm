d94469f527e38510ada97276c41776df81b9ffa9
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2008-03-17 01:28:48 +0000

    lvchange had a seperate class for each tab. Now it has just one class for the whole dialog.

--- trunk/kvpm/lvchange.h
+++ trunk/kvpm/lvchange.h
@@ -11,50 +11,49 @@
  * 
  * See the file "COPYING" for the exact licensing terms.
  */
+
 #ifndef LVCHANGE_H
 #define LVCHANGE_H
 
 #include <KDialog>
 #include <KTabWidget>
+#include <KLineEdit>
+
 #include <QStringList>
 #include <QGroupBox>
 #include <QCheckBox>
-#include <QLineEdit>
 
 class LogVol;
 
-bool change_lv(LogVol *LogicalVolumes);
+bool change_lv(LogVol *logicalVolumes);
 
-class LVChangeGeneralTab : public QWidget
-{
- public:
-    LVChangeGeneralTab(LogVol *lv, QWidget *parent = 0);
-    QCheckBox *available_check, *contig_check;
-    QCheckBox *ro_check, *refresh_check;
-    
-};
-
-class LVChangeAdvancedTab : public QWidget
-{
- public:
-    LVChangeAdvancedTab(LogVol *lv, QWidget *parent = 0);
-    QGroupBox *persistant_box, *mirror_box;
-    QLineEdit *minor_edit, *major_edit;
-    QCheckBox *resync_check, *monitor_check;
-
-};
 
 class LVChangeDialog : public KDialog
 {
 Q_OBJECT
-    KTabWidget *tab_widget;
-    QGroupBox *group_box;
-    QString lv_full_name;
-    LVChangeGeneralTab *general_tab;
-    LVChangeAdvancedTab *advanced_tab;
+
+    LogVol *m_lv;
+    
+    QCheckBox *available_check,   // Make the volume available 
+	      *contig_check,      // allocate contiguous extents
+	      *ro_check,          // make the volume read only
+	      *refresh_check,     // refresh the metadata
+	      *resync_check,      // re-sync mirrors
+	      *monitor_check;     // dmeventd monitoring
+
+    QGroupBox *m_persistant_box, *m_mirror_box;
+
+    KLineEdit *minor_edit,  // User entered device minor number 
+              *major_edit;  // User entered device major number 
+
+    QWidget *m_general_tab;
+    QWidget *m_advanced_tab;
+    
+    void buildGeneralTab();
+    void buildAdvancedTab();
     
 public:
-    LVChangeDialog(LogVol *LogicalVolume, QWidget *parent = 0);
+    LVChangeDialog(LogVol *logicalVolume, QWidget *parent = 0);
     QStringList arguments();
     
 };
