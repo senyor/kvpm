db3107dbec2bec3c51d6c0baf1c22d78c290003e
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2008-03-19 00:19:38 +0000

    Bumped up the version up to 0.4.3 and add more information the the pvproperties widgets. They now use a QTableWidget to show the extents used by each logical volume.

--- kvpm/pvproperties.cpp
+++ kvpm/pvproperties.cpp
@@ -27,44 +27,102 @@ extern MasterList *master_list;
 PVProperties::PVProperties(PhysVol *physicalVolume, QWidget *parent):QWidget(parent)
 {
     VolGroup *vg = master_list->getVolGroupByName( physicalVolume->getVolumeGroupName() ) ;
+    int segment_count;
+    LogVol *lv;
 
+    QTableWidgetItem *new_item;
+    
+    long long first_extent;
+    long long last_extent;
+    
     QList<LogVol *>  lvs = vg->getLogicalVolumes();
     QList<PhysVol *> pvs = vg->getPhysicalVolumes();
-
-    QStringList lv_name_list;
+    QList<long long> starting_extent;
     QStringList pv_name_list;
     QString device_name = physicalVolume->getDeviceName();
-    
 
     QVBoxLayout *layout = new QVBoxLayout();
     setLayout(layout);
 
+    QLabel *pv_name_label = new QLabel( "<b>" + device_name + "</b>");
+    pv_name_label->setAlignment(Qt::AlignHCenter);
+    layout->addWidget(pv_name_label);
+    
+    QTableWidget *table_widget = new QTableWidget();
+    layout->addWidget(table_widget);
+
+    QStringList headers;
+    headers << "Logical Volume" 
+	    << "Start" 
+	    << "End" 
+	    << "Extents" 
+	    << "";
+
+    table_widget->setColumnCount(5);
+    table_widget->setRowCount(0);
+    
+
+    table_widget->setFrameShape(QFrame::NoFrame);
+    setBackgroundRole(QPalette::Base);
+    setAutoFillBackground(true);
+    pv_name_label->setAutoFillBackground(true);
+    pv_name_label->setBackgroundRole(QPalette::Base);
+
+    QLabel *uuid_label = new QLabel("UUID: " + physicalVolume->getUuid() );
+    uuid_label->setAutoFillBackground(true);
+    uuid_label->setBackgroundRole(QPalette::Base);
+    uuid_label->setWordWrap(true);
+    layout->addWidget(uuid_label);
+    
 
 /* here we get the names of logical volumes associated
    with the physical volume */
 
-
+    int row = 0;
+    
     for(int x = 0; x < lvs.size() ; x++){
-	pv_name_list = lvs[x]->getDevicePathAll();
-	for(int y = 0; y < pv_name_list.size() ; y++)
-	    if( device_name == pv_name_list[y] )
-		lv_name_list.append( lvs[x]->getName() );
+	lv = lvs[x];
+	segment_count = lv->getSegmentCount();
+	for(int segment = 0; segment < segment_count; segment++){
+	    pv_name_list = lv->getDevicePath(segment);
+	    starting_extent = lv->getSegmentStartingExtent(segment);
+	    for(int y = 0; y < pv_name_list.size() ; y++){
+		if( device_name == pv_name_list[y] ){
+		    first_extent = starting_extent[y];
+		    last_extent = first_extent - 1 + (lv->getSegmentExtents(segment) / (lv->getSegmentStripes(segment)));
+		    
+		    table_widget->insertRow( row );
+		    
+		    new_item = new QTableWidgetItem( lv->getName() );
+		    table_widget->setItem(row, 0, new_item);
+		    
+		    new_item = new QTableWidgetItem( QString("%1").arg(first_extent) );
+		    new_item->setTextAlignment(Qt::AlignRight);
+		    table_widget->setItem(row, 1, new_item);
+		    new_item = new QTableWidgetItem( QString("%1").arg(last_extent) );
+		    new_item->setTextAlignment(Qt::AlignRight);
+		    table_widget->setItem(row, 2, new_item);
+		    new_item = new QTableWidgetItem( QString("%1").arg(last_extent - first_extent + 1 ) );
+		    new_item->setTextAlignment(Qt::AlignRight);
+		    
+		    table_widget->setItem(row, 3, new_item);
+		    row++;
+		}
+	    }
+	}
     }
-    
-/* next we remove duplicate entries */
 
-    lv_name_list.sort();
-    if( lv_name_list.size() > 1 )
-	for(int x = lv_name_list.size() - 2; x >= 0; x--)
-	    if( lv_name_list[x] == lv_name_list[x + 1] )
-		lv_name_list.removeAt(x + 1);
+    table_widget->setHorizontalHeaderLabels(headers);
+    table_widget->resizeColumnToContents(0);
+    table_widget->resizeColumnToContents(1);
+    table_widget->resizeColumnToContents(2);
+    table_widget->resizeColumnToContents(3);
+    table_widget->setShowGrid(false);
+    table_widget->setAlternatingRowColors(true);
+    table_widget->verticalHeader()->hide();
 
+    table_widget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    table_widget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    layout->setSizeConstraint(QLayout::SetFixedSize);
 
-    layout->addWidget( new QLabel( "<b>" + device_name + "</b>" ) );
-    
-    for(int x = 0; x < lv_name_list.size(); x++)
-	layout->addWidget( new QLabel( lv_name_list[x] ) );
-
-    layout->addStretch();
-    
-}
+} 
