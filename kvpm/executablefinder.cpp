a10631aade5174bf6a6eb6c17a1ccefffbd0760f
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2009-01-25 03:39:07 +0000

    The configure settings window now is somewhat functional.

--- kvpm/executablefinder.cpp
+++ kvpm/executablefinder.cpp
@@ -29,70 +29,44 @@
 
 ExecutableFinder::ExecutableFinder(QObject *parent) : QObject(parent)
 {
-    struct stat buf;
-    QStringList keys;
-    QStringList default_search_paths;
-    const char *path;
-
-    keys << "lvchange"
-	 << "lvconvert" 
-	 << "lvcreate"
-	 << "lvextend"
-	 << "lvreduce"
-	 << "lvremove"
-	 << "lvrename"
-	 << "lvs"
-	 << "mkfs"
-	 << "mkswap"
-	 << "pvchange"
-	 << "pvcreate"
-	 << "pvmove" 
-	 << "pvremove"
-	 << "pvs"
-	 << "resize2fs" 
-	 << "resize_reiserfs"
-	 << "vgchange"
-	 << "vgcreate"
-	 << "vgexport"
-	 << "vgextend"
-	 << "vgimport"
-	 << "vgreduce"
-	 << "vgremove"
-	 << "vgrename"
-	 << "vgs" 
-	 << "xfs_growfs";
-    
-    
-    default_search_paths << "/sbin/" 
-			 << "/usr/sbin/" 
-			 << "/bin/" 
-			 << "/usr/bin/" 
-			 << "/usr/local/bin/"
-			 << "/usr/local/sbin/";
+    m_keys << "lvchange"
+	   << "lvconvert" 
+	   << "lvcreate"
+	   << "lvextend"
+	   << "lvreduce"
+	   << "lvremove"
+	   << "lvrename"
+	   << "lvs"
+	   << "mkfs"
+	   << "mkswap"
+	   << "pvchange"
+	   << "pvcreate"
+	   << "pvmove" 
+	   << "pvremove"
+	   << "pvs"
+	   << "resize2fs" 
+	   << "resize_reiserfs"
+	   << "vgchange"
+	   << "vgcreate"
+	   << "vgexport"
+	   << "vgextend"
+	   << "vgimport"
+	   << "vgreduce"
+	   << "vgremove"
+	   << "vgrename"
+	   << "vgs" 
+	   << "xfs_growfs";
     
 
-    // Read the kvpmrc files and get the search paths or use the default
-    // if none are found, then write out the default. 
 
-    KConfig kvpm_config( "kvpmrc", KConfig::SimpleConfig );
-    KConfigGroup system_paths_group( &kvpm_config, "SystemPaths" );
-
-    QStringList search_paths = system_paths_group.readEntry( "SearchPath", QStringList() );
-    if( search_paths.isEmpty() ){
-        search_paths = default_search_paths ;
-        system_paths_group.writeEntry( "SearchPath",    search_paths );
-        system_paths_group.sync();
-    }
-
-    for(int y = 0; y < keys.size(); y++){
-	for(int x = 0; x < search_paths.size(); x++){
-	    path = QString( search_paths[x] + keys[y] ).toAscii().data();
-	    if( lstat(path, &buf) == 0 ){
-		m_path_map.insert( keys[y], search_paths[x] + keys[y] );
-		break;
-	    }
-	}
-    }
+    m_default_search_paths << "/sbin/" 
+			   << "/usr/sbin/" 
+			   << "/bin/" 
+			   << "/usr/bin/" 
+			   << "/usr/local/bin/"
+			   << "/usr/local/sbin/";
+    
+    reload();
 
 }
 
@@ -104,4 +78,59 @@ QString ExecutableFinder::getExecutablePath(QString name)
 	qDebug() << "Excutable Finder: error " << name << " does not map to any path";
     
     return path;
+}
+
+void ExecutableFinder::reload()
+{
+    struct stat buf;
+    const char *path;
+    int key_length = m_keys.size();
+
+    // Read the kvpmrc files and get the search paths or use the default
+    // if none are found, then write out the default. 
+
+    KConfig kvpm_config( "kvpmrc", KConfig::SimpleConfig );
+    KConfigGroup system_paths_group( &kvpm_config, "SystemPaths" );
+
+    QStringList search_paths = system_paths_group.readEntry( "SearchPath", QStringList() );
+    if( search_paths.isEmpty() ){
+        search_paths = m_default_search_paths ;
+        system_paths_group.writeEntry( "SearchPath",    search_paths );
+        system_paths_group.sync();
+    }
+
+    m_path_map.clear();
+
+    for(int y = 0; y < key_length; y++){
+	for(int x = 0; x < search_paths.size(); x++){
+	    path = QString( search_paths[x] + m_keys[y] ).toAscii().data();
+	    if( lstat(path, &buf) == 0 ){
+		m_path_map.insert( m_keys[y], search_paths[x] + m_keys[y] );
+		break;
+	    }
+	}
+    }
+
+    m_not_found.clear();
+    for(int x = 0; x < key_length; x++){
+        if( m_path_map.value( m_keys[x] ) == "" )
+	    m_not_found.append( m_keys[x] );
+    }
+}
+
+
+QStringList ExecutableFinder::getAllPaths()
+{
+    return m_path_map.values();
+}
+
+
+QStringList ExecutableFinder::getAllNames()
+{
+    return   m_path_map.keys();
+}
+
+QStringList ExecutableFinder::getNotFound()
+{
+    return m_not_found;
 }
--- kvpm/kvpmconfigdialog.h
+++ kvpm/kvpmconfigdialog.h
@@ -4,8 +4,8 @@
 
 
 class KConfigSkeleton;
-
-
+class KEditListBox;
+class QTableWidget;
 
 bool config_kvpm();
 
@@ -14,19 +14,29 @@ class KvpmConfigDialog: public KConfigDialog
 {
 Q_OBJECT
 
+    QTableWidget    *m_executables_table;
+    KConfig         *m_kvpm_config;
+    KConfigGroup    *m_system_paths_group;
+    KEditListBox    *m_edit_list;
+    KConfigSkeleton *m_skeleton;
+    QStringList      m_search_entries;
 
-    KConfig      *m_kvpm_config;
-    KConfigGroup *m_system_paths_group;
-
+    void buildGeneralPage();
+    void buildColorsPage();
     void buildProgramsPage();
+
 
 public:
 
     KvpmConfigDialog( QWidget *parent, QString name, KConfigSkeleton *skeleton  );
     ~KvpmConfigDialog();
 
-private slots:
+public slots:
     void updateSettings();
-
+    void updateWidgetsDefault();
+    void updateConfig();
+    void fillExecutablesTable();
+    bool isDefault();
+    bool hasChanged();
 };
 
