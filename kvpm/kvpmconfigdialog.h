f3fe4314017504f7a45a4d6afdcd6888de4ce80e
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2009-01-27 05:02:04 +0000

    The dialog now sets the colors for filesystems on the graphics, except the graphic end doesn't read them yet.

--- kvpm/kvpmconfigdialog.h
+++ kvpm/kvpmconfigdialog.h
@@ -1,11 +1,11 @@
 #include <KConfigDialog>
+#include <KConfigSkeleton>
+#include <KColorButton>
+#include <KEditListBox>
 
-#include <QString>
-
-
-class KConfigSkeleton;
-class KEditListBox;
-class QTableWidget;
+#include <QTableWidget>
+#include <QStringList>
+#include <QColor>
 
 bool config_kvpm();
 
@@ -21,6 +21,31 @@ Q_OBJECT
     KConfigSkeleton *m_skeleton;
     QStringList      m_search_entries;
 
+    KColorButton *m_ext2_button,
+                 *m_ext3_button,
+                 *m_ext4_button,
+                 *m_reiser_button,
+                 *m_reiser4_button,
+                 *m_msdos_button,
+                 *m_swap_button,
+                 *m_xfs_button,
+                 *m_jfs_button,
+                 *m_free_button,
+                 *m_none_button;
+
+    QColor m_ext2_color,
+           m_ext3_color,
+           m_ext4_color,
+           m_reiser_color,
+           m_reiser4_color,
+           m_msdos_color,
+           m_swap_color,
+           m_xfs_color,
+           m_jfs_color,
+           m_free_color,
+           m_none_color;
+
+
     void buildGeneralPage();
     void buildColorsPage();
     void buildProgramsPage();
@@ -34,7 +59,7 @@ public:
 public slots:
     void updateSettings();
     void updateWidgetsDefault();
-    void updateConfig();
+    //    void updateConfig();
     void fillExecutablesTable();
     bool isDefault();
     bool hasChanged();
