eeabff60a2bcf17390a6328744c5b5b62217d645
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2009-03-09 21:07:31 +0000

    updated the copyright notice

--- trunk/kvpm/kvpmconfigdialog.h
+++ trunk/kvpm/kvpmconfigdialog.h
@@ -1,3 +1,18 @@
+/*
+ *
+ * 
+ * Copyright (C) 2009 Benjamin Scott   <benscott@nwlink.com>
+ *
+ * This file is part of the Kvpm project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License,  version 3, as 
+ * published by the Free Software Foundation.
+ * 
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+
 #include <KConfigDialog>
 #include <KConfigSkeleton>
 #include <KColorButton>
