c322e33f932d1d7035d80defeeb6a9c5ad272f1c
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2010-07-22 03:49:35 +0000

    converted vgremove to use lvm2app

--- trunk/kvpm/vgremove.cpp
+++ trunk/kvpm/vgremove.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2008 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2008, 2010 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the Kvpm project.
  *
@@ -12,7 +12,8 @@
  * See the file "COPYING" for the exact licensing terms.
  */
 
-
+#include <lvm2app.h>
+ 
 #include <KMessageBox>
 #include <KLocale>
 #include <QtGui>
@@ -23,23 +24,31 @@
 
 bool remove_vg(VolGroup *volumeGroup)
 {
-    QStringList args;
     QString message;
+    lvm_t  lvm = NULL;
+    vg_t vg_dm = NULL;
 
     message = i18n("Are you certain you want to "
 		   "delete volume group: <b>%1</b>").arg(volumeGroup->getName());
 
-
     if(KMessageBox::questionYesNo( 0, message) == 3){      // 3 = "yes" button
-    
-	args << "vgremove"
-	     << volumeGroup->getName();
-
-	ProcessProgress remove(args, i18n("Removing volume group...") );
-	
-	return true;
+        if( (lvm = lvm_init(NULL)) ){
+            if( (vg_dm = lvm_vg_open(lvm, volumeGroup->getName().toAscii().data(), "w", NULL)) ){
+                if( lvm_vg_remove(vg_dm) ) 
+                    KMessageBox::error(0, QString(lvm_errmsg(lvm)));
+                else{
+                    if( lvm_vg_write(vg_dm) )
+                        KMessageBox::error(0, QString(lvm_errmsg(lvm)));
+                }
+                lvm_vg_close(vg_dm);
+            }
+            else
+                KMessageBox::error(0, QString(lvm_errmsg(lvm)));
+            lvm_quit(lvm);
+        }
+        else
+            KMessageBox::error(0, QString(lvm_errmsg(lvm)));
+        return true;
     }
-    else{		   
-	return false;
-    }
+    return false;
 }
