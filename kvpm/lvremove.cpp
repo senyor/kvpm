0f0a6cf1f8c5f87c4e8579f1fe442f49eceb8192
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2010-08-14 05:23:51 +0000

    Changed lvremove (again!) with regard to deleting snapshots. All snaps must be removed before removing the origin. No attempt is made to deactivate the snap defore deleting.

--- kvpm/lvremove.cpp
+++ kvpm/lvremove.cpp
@@ -25,33 +25,35 @@
 bool remove_lv(LogVol *logicalVolume)
 {
     QString full_name = logicalVolume->getFullName();
+    full_name.remove('[').remove(']');
     QStringList args;
     
     QString message = i18n("Are you certain you want to delete the logical volume named: %1 "
 			   "Any data on it will be lost.").arg("<b>" + full_name + "</b>");
 
-    full_name.remove('[').remove(']');
+    QString message2 = i18n("This volume has snapshots that must be deleted first");
 
-    if( KMessageBox::warningYesNo( 0, message) == 3){  // 3 = yes button
+    if( logicalVolume->isOrigin() ){
+        KMessageBox::error( 0, message2);
+        return false; 
+    }
+
+    if(KMessageBox::warningYesNo( 0, message) == 3){  // 3 = yes button
         
-        if( logicalVolume->isActive() && !logicalVolume->isSnap() && !logicalVolume->isOrigin() ){
-            args << "lvchange" 
-                 << "-an" 
-                 << full_name;
+        if( logicalVolume->isActive() && !logicalVolume->isSnap() ){
+            args << "lvchange" << "-an" << full_name;
 
-	ProcessProgress deactive( args, i18n("Deactivating volume..."), false);
+            ProcessProgress deactive( args, i18n("Deactivating volume..."), true);
         }
-        
+
         args.clear();
-	args << "lvremove" 
-	     << "--force" 
-	     << full_name;
-
-	ProcessProgress remove( args, i18n("Removing volume..."), false);
-
-	return true;
+        args << "lvremove" << "--force" << full_name;
+        
+        ProcessProgress remove( args, i18n("Removing volume..."), true);
+        
+        return true;
     }
     else
-	return false;
+        return false;
 }
 
