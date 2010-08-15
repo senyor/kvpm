701a11864719e1f59bca5b4f1573313261251139
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2010-08-15 22:52:42 +0000

    Added progress dialog for vgremove

--- kvpm/vgremove.cpp
+++ kvpm/vgremove.cpp
@@ -16,9 +16,9 @@
  
 #include <KMessageBox>
 #include <KLocale>
+#include <KProgressDialog>
 #include <QtGui>
 
-#include "processprogress.h"
 #include "vgremove.h"
 #include "volgroup.h"
 
@@ -27,13 +27,28 @@ bool remove_vg(VolGroup *volumeGroup)
     QString message;
     lvm_t  lvm = NULL;
     vg_t vg_dm = NULL;
+    KProgressDialog *progress_dialog;
 
     message = i18n("Are you certain you want to "
 		   "delete volume group: <b>%1</b>").arg(volumeGroup->getName());
 
     if(KMessageBox::questionYesNo( 0, message) == 3){      // 3 = "yes" button
+
+        progress_dialog = new KProgressDialog(NULL, i18n("progress"), i18n("deleting group"));
+        progress_dialog->setAllowCancel(false);
+        progress_dialog->setMinimumDuration(250); 
+        QProgressBar *progress_bar = progress_dialog->progressBar();
+        progress_bar->setRange(0,3);
+        progress_bar->setValue(1);
+        progress_dialog->show();
+        qApp->processEvents();
+                
         if( (lvm = lvm_init(NULL)) ){
+            progress_bar->setValue(2);
+            qApp->processEvents();
             if( (vg_dm = lvm_vg_open(lvm, volumeGroup->getName().toAscii().data(), "w", NULL)) ){
+                progress_bar->setValue(2);
+                qApp->processEvents();
                 if( lvm_vg_remove(vg_dm) ) 
                     KMessageBox::error(0, QString(lvm_errmsg(lvm)));
                 else{
@@ -41,6 +56,8 @@ bool remove_vg(VolGroup *volumeGroup)
                         KMessageBox::error(0, QString(lvm_errmsg(lvm)));
                 }
                 lvm_vg_close(vg_dm);
+                progress_bar->setValue(3);
+                qApp->processEvents();
             }
             else
                 KMessageBox::error(0, QString(lvm_errmsg(lvm)));
@@ -48,6 +65,10 @@ bool remove_vg(VolGroup *volumeGroup)
         }
         else
             KMessageBox::error(0, QString(lvm_errmsg(lvm)));
+
+        progress_dialog->close();
+        progress_dialog->delayedDestruct();
+        qApp->processEvents();
         return true;
     }
     return false;
