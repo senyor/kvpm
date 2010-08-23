d1f8cfe1df2f82cb7fefb803a53b0686c93c5cc0
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2010-08-23 20:32:34 +0000

    The usage of dumpe2fs to get fsdata was a failure. This time I have used statvfs with much better success.

--- kvpm/fsdata.cpp
+++ kvpm/fsdata.cpp
@@ -12,70 +12,42 @@
  * See the file "COPYING" for the exact licensing terms.
  */
 
+#include <errno.h>
+#include <string.h>
+#include <sys/statvfs.h>
+
 #include <KLocale>
 #include <KMessageBox>
 
 #include <QtGui>
 
 #include "fsdata.h"
-#include "processprogress.h"
 #include "misc.h"
 
 FSData *get_fs_data(QString path){
 
-    QStringList arguments, 
-                output,
-                temp_stringlist;
-
-    QString block_string;
-
-    long long block_size = 0;
-    long long total_blocks = 0;
-    long long used_blocks = 0;
-
-    arguments << "dumpe2fs" << "-h" << path;
-
-    ProcessProgress size_scan(arguments, i18n("Checking size"), false );
-    output = size_scan.programOutput();
-
-    temp_stringlist << output.filter("Block size", Qt::CaseInsensitive);
-
-    if( temp_stringlist.size() ){
-        block_string = temp_stringlist[0];
-        block_string = block_string.remove( 0, block_string.indexOf(":") + 1 );
-        block_string = block_string.simplified();
-        block_size = block_string.toLong();
-    }
-
-    temp_stringlist.clear();
-
-    temp_stringlist << output.filter("Block count", Qt::CaseInsensitive);
-    if( temp_stringlist.size() && block_size > 0 ){
-        block_string = temp_stringlist[0];
-        block_string = block_string.remove( 0, block_string.indexOf(":") + 1 );
-        block_string = block_string.simplified();
-        total_blocks = block_string.toLong();
-    }
-
-    temp_stringlist.clear();
-
-    temp_stringlist << output.filter("Free blocks", Qt::CaseInsensitive);
-    if( temp_stringlist.size() && total_blocks > 0 ){
-        block_string = temp_stringlist[0];
-        block_string = block_string.remove( 0, block_string.indexOf(":") + 1 );
-        block_string = block_string.simplified();
-        used_blocks  = total_blocks - block_string.toLong();
-    }
+    long long total_blocks, frag_size, block_size;
+    int error;
 
     FSData *fs_data = new FSData();
     fs_data->size = -1;
     fs_data->used = -1;
     fs_data->block_size = -1;
 
-    if( block_size > 0 && total_blocks > 0 ){
-        fs_data->size = block_size * total_blocks;
-        fs_data->used = block_size * used_blocks;
-        fs_data->block_size = block_size;
+    struct statvfs *buf;
+    buf = new struct statvfs;
+
+    const char *mp = path.toAscii().data();
+    error =  statvfs(mp, buf);
+ 
+    if(!error){  // We use "long long" intermediate variables here
+        block_size = buf->f_bsize;
+        frag_size = buf->f_frsize;
+        total_blocks = (frag_size * buf->f_blocks) / block_size;
+
+        fs_data->block_size = block_size; 
+        fs_data->size = total_blocks;
+        fs_data->used = total_blocks - buf->f_bavail;
     }
 
     return fs_data;
