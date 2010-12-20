6989ca2525a2cc5190421d0e9699c50f11044a29
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2010-12-20 07:11:50 +0000

    Changed vgreduce to use the PVCheckbox object so selected pv are handled consistently.

--- kvpm/vgreduce.cpp
+++ kvpm/vgreduce.cpp
@@ -18,6 +18,7 @@
 #include <KLocale>
 #include <QtGui>
 
+#include "pvcheckbox.h"
 #include "masterlist.h"
 #include "misc.h"
 #include "vgreduce.h"
@@ -48,50 +49,39 @@ VGReduceDialog::VGReduceDialog(VolGroup *volumeGroup, QWidget *parent) : KDialog
     m_unremovable_pvs_present = false;
     QString vg_name = m_vg->getName();
 
-    QLabel *label = new QLabel( i18n( "Select any of the following physical volumes to "
-				      "remove them from volume group <b>%1</b>").arg(vg_name));
+    QList<PhysVol *> member_pvs = m_vg->getPhysicalVolumes();
+    int pv_count = m_vg->getPhysVolCount(); 
+    m_unremovable_pvs_present = false;
+
+    for(int x = pv_count - 1; x >= 0; x--){
+	if( member_pvs[x]->getSize() - member_pvs[x]->getUnused() ){ // only unused pvs can be removed
+            member_pvs.removeAt(x);
+            m_unremovable_pvs_present = true;
+        }
+    }
+
+    QLabel *label;
+    if(m_unremovable_pvs_present){
+        label = new QLabel( i18n( "Select physical volumes to "
+                                  "remove them from volume group <b>%1</b>").arg(vg_name));
+    }
+    else{
+        label = new QLabel( i18n( "Select physical volumes <b>excluding one</b> to "
+                                  "remove them from volume group <b>%1</b>").arg(vg_name));
+    }
 
     label->setWordWrap(true);
     layout->addWidget(label);
 
-    QGroupBox *pv_unused_box = new QGroupBox( i18n("Unused physical volumes") );
-    QVBoxLayout *pv_unused_layout = new QVBoxLayout;
-    pv_unused_box->setLayout(pv_unused_layout);
-    layout->addWidget(pv_unused_box);
+    m_pv_checkbox = new PVCheckBox(member_pvs, m_vg->getExtentSize());
+    layout->addWidget(m_pv_checkbox);
+    m_pv_checkbox->setTitle( i18n("Unused physical volumes") );
 
-    QGroupBox *pv_used_box = new QGroupBox( i18n("In use physical volumes") );
-    QVBoxLayout *pv_used_layout = new QVBoxLayout;
-    pv_used_box->setLayout(pv_used_layout);
-
-    QList<PhysVol *> member_pvs = m_vg->getPhysicalVolumes();
-    int pv_count = m_vg->getPhysVolCount(); 
-    NoMungeCheck *pv_check = NULL;
-    
-    for(int x = 0; x < pv_count; x++){
-	if( !( member_pvs[x]->getSize() - member_pvs[x]->getUnused() ) ){ // only unused pvs can be removed
-	    pv_check = new NoMungeCheck( member_pvs[x]->getDeviceName() );
-	    m_pv_check_boxes.append(pv_check);
-	    pv_unused_layout->addWidget(pv_check);
-	    connect(pv_check, SIGNAL(toggled(bool)), this, SLOT(excludeOneVolume(bool)));
-	}
-	else{
-	    m_unremovable_pvs_present = true;
-	    pv_used_layout->addWidget(new QLabel(member_pvs[x]->getDeviceName()));
-	}
-    }
-
-    if(	!m_pv_check_boxes.size() )                             // no unused pvs present
-        pv_unused_layout->addWidget( new QLabel( i18n("none") ) );
-    
-    if(m_unremovable_pvs_present){
-        layout->addWidget( new QLabel( i18n("<b>The following may not be removed</b>") ) );
-	layout->addWidget(pv_used_box);
-    }
+    connect(m_pv_checkbox, SIGNAL(stateChanged()), this, SLOT(excludeOneVolume()));
+    m_pv_checkbox->selectNone();
 
     connect(this, SIGNAL(okClicked()), 
             this, SLOT(commitChanges()));
-
-    enableButtonOk(false);
 }
 
 void VGReduceDialog::commitChanges()
@@ -101,13 +91,8 @@ void VGReduceDialog::commitChanges()
     vg_t vg_dm = NULL;
 
     QStringList pv_list; // pvs to remove by name
-    int check_box_count = m_pv_check_boxes.size();
+    pv_list << m_pv_checkbox->getNames();
 
-    for(int x = 0; x < check_box_count; x++){
-	if(m_pv_check_boxes[x]->isChecked())
-	    pv_list << m_pv_check_boxes[x]->getUnmungedText();
-    }
-    
     if( (lvm = lvm_init(NULL)) ){
         if( (vg_dm = lvm_vg_open(lvm, m_vg->getName().toAscii().data(), "w", NULL)) ){
             for(int x = 0; x < pv_list.size(); x++){
@@ -128,32 +113,21 @@ void VGReduceDialog::commitChanges()
     return;
 }
 
-void VGReduceDialog::excludeOneVolume(bool)
+void VGReduceDialog::excludeOneVolume()
 {
-    int boxes_checked = 0;
-    int check_box_count = m_pv_check_boxes.size();
-    
-    bool selection_made = false;   // True if a least one pv is checked (selected)
-    
-    for(int x = 0; x < check_box_count; x++){
-	if(m_pv_check_boxes[x]->isChecked()){
-	    selection_made = true;
-	    boxes_checked++;
-	}
-    }
-    
-    enableButtonOk(selection_made); // enable the OK button only if at least one pv is selected
+    QStringList names = m_pv_checkbox->getNames();
+    QStringList all_names = m_pv_checkbox->getAllNames();
+    int boxes_checked = names.size();
+    int boxes_count   = all_names.size();
 
-    if( ((boxes_checked + 1) == check_box_count ) && (!m_unremovable_pvs_present) ){
-	
-	for(int x = 0; x < check_box_count; x++){
-	    if( !(m_pv_check_boxes[x]->isChecked()) )
-		m_pv_check_boxes[x]->setEnabled(false);
-	}
+    if(boxes_checked > 0){
+        if(m_unremovable_pvs_present || (boxes_checked < boxes_count))    
+            enableButtonOk(true);
+        else
+            enableButtonOk(false);
     }
-    else{
-	for(int x = 0; x < check_box_count; x++)
-	    m_pv_check_boxes[x]->setEnabled(true);
-    }
+    else
+        enableButtonOk(false);
 }
+
 
