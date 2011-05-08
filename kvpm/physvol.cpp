77e2d19f022b8c5d456cb4741543034d7139dc79
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-05-08 23:31:56 +0000

    updated copy right

--- kvpm/physvol.cpp
+++ kvpm/physvol.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2008, 2010 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2008, 2010, 2011 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the kvpm project.
  *
