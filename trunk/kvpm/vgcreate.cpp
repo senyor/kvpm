2a5dea9d81b7fec1c39bc85167f348e615b0bf1b
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-05-16 18:52:16 +0000

    Enable creation of clustered groups

--- trunk/kvpm/vgcreate.cpp
+++ trunk/kvpm/vgcreate.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2008, 2009, 2010 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2008, 2009, 2010, 2011 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the kvpm project.
  *
@@ -216,7 +216,6 @@ VGCreateDialog::VGCreateDialog(QList<AvailableDevice *> devices, QWidget *parent
     pv_layout_h->addWidget(m_max_pvs);
 
     m_clustered = new QCheckBox( i18n("Cluster Aware") );
-    m_clustered->setEnabled(false);
     
     m_auto_backup = new QCheckBox( i18n("Automatic Backup") );
     m_auto_backup->setCheckState(Qt::Checked);
