cc5c3cbdc7b8439c06733dbdbcd32a77e6af99d6
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-05-18 03:17:05 +0000

    All of the vgchange dialogs got compressed into a single dialog now that the "vgchange" program take multiple parameters.

--- kvpm/topwindow.h
+++ kvpm/topwindow.h
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2008, 2009, 2010 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2008, 2009, 2010, 2011 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the kvpm project.
  *
@@ -38,15 +38,11 @@ Q_OBJECT
 
     KAction *quit_action,            *remove_vg_action,       *rename_vg_action, 
             *reduce_vg_action,       *rescan_action,          *create_vg_action,
-	    *vgchange_alloc_action,  *vgchange_pv_action,     
-	    *vgchange_lv_action,     *vgchange_resize_action, *vgchange_available_action, 
-            *vgchange_extent_action, *remove_missing_action,  *config_kvpm_action,
+            *change_vg_action,	     *remove_missing_action,  *config_kvpm_action,
             *restart_pvmove_action,  *stop_pvmove_action,     *extend_vg_action,
 	    *export_vg_action,       *import_vg_action,
             *split_vg_action,        *merge_vg_action;
     
-    KMenu *m_vgchange_menu;
-
     DeviceTab *m_device_tab;
     VolGroup  *m_vg;
 
@@ -58,12 +54,7 @@ Q_OBJECT
  
  private slots:
     void setupMenus();
-    void changeAllocation();
-    void changeAvailable();
-    void changeExtentSize();
-    void limitLogicalVolumes();
-    void limitPhysicalVolumes();
-    void changeResize();
+    void changeVolumeGroup();
     void createVolumeGroup();
     void removeVolumeGroup();
     void renameVolumeGroup();
