42e3ff449c6faaf9fc7d44b2a3b00834327891e1
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-05-21 22:58:00 +0000

    Added an alternate text list (string list) to the data that can be stored in NoMungeCheck.

--- trunk/kvpm/misc.h
+++ trunk/kvpm/misc.h
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2008, 2010 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2008, 2010, 2011 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the Kvpm project.
  *
@@ -30,15 +30,18 @@ struct AvailableDevice {
 
 class NoMungeCheck : public QCheckBox
 {
-    QString m_unmunged_text;    // QCheckBox text() without amperands
-    QString m_alternate_text;   // We can put anything we want in here
-    QVariant m_data;
+    QString     m_unmunged_text;    // QCheckBox text() without amperands
+    QString     m_alternate_text;   // We can put anything we want in here
+    QStringList m_alternate_text_list;   // Ditto
+    QVariant    m_data;
 
  public:
     NoMungeCheck(const QString text, QWidget *parent = NULL);
-    QString getAlternateText();
-    QString getUnmungedText();
+    QString     getAlternateText();
+    QStringList getAlternateTextList();
+    QString     getUnmungedText();
     void setAlternateText(QString alternateText);
+    void setAlternateTextList(QStringList alternateTextList);
     void setData(QVariant data);
     QVariant getData();    
 };
