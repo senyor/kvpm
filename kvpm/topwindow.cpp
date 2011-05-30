cabbb2cdd1b448c3b2ba54a270a187829ff87c1d
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-05-30 19:54:02 +0000

    Version bump to 0.7.3

--- kvpm/topwindow.cpp
+++ kvpm/topwindow.cpp
@@ -57,7 +57,7 @@ TopWindow::TopWindow(QWidget *parent):KMainWindow(parent)
     KAboutData *about_data = new KAboutData( QByteArray("kvpm"),
 					     QByteArray(""),
 					     ki18n("kvpm"),
-					     QByteArray("0.7.2"),
+					     QByteArray("0.7.3"),
 					     ki18n("Linux volume and partition manager for KDE.\n"
 						   "This program is still under development,\n"
 						   "bug reports and any comments are welcomed.\n"),
