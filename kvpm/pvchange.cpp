659816c21a544de3807d4ad6786931d491302703
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-06-30 00:15:50 +0000

    added ability to ignore or use metadata areas and to generate a new uuid for a pv.

--- kvpm/pvchange.cpp
+++ kvpm/pvchange.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2008 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2008, 2011 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the kvpm project.
  *
@@ -17,12 +17,12 @@
 #include <QtGui>
 
 #include "pvchange.h"
+#include "physvol.h"
 
 
-PVChangeDialog::PVChangeDialog(QString physicalVolumePath, QWidget *parent):
+PVChangeDialog::PVChangeDialog(PhysVol *physicalVolume, QWidget *parent):
     KDialog(parent),
-    m_pv_path(physicalVolumePath)
-
+    m_pv(physicalVolume)
 {
 
     setWindowTitle( i18n("Change physical volume attributes") );
@@ -32,30 +32,64 @@ PVChangeDialog::PVChangeDialog(QString physicalVolumePath, QWidget *parent):
     QVBoxLayout *layout = new QVBoxLayout;
     dialog_body->setLayout(layout);
 
-    QLabel *label = new QLabel( i18n("Physical volume: %1").arg(m_pv_path) );
+    QString pv_name = m_pv->getName();
+    QLabel *label = new QLabel( i18n("Physical volume: %1").arg(pv_name) );
     layout->addWidget(label);
     QGroupBox *attrib_box = new QGroupBox( i18n("Attributes") );
     QVBoxLayout *attrib_box_layout = new QVBoxLayout;
     attrib_box->setLayout(attrib_box_layout);
     layout->addWidget(attrib_box);
-    allocation_box = new QCheckBox( i18n("Enable allocation of extents") );
-    attrib_box_layout->addWidget(allocation_box);
+    m_allocation_box = new QCheckBox( i18n("Enable allocation of extents") );
+    if( m_pv->isAllocatable() )
+        m_allocation_box->setChecked(true);
+    attrib_box_layout->addWidget(m_allocation_box);
+    m_mda_box = new QCheckBox( i18n("Use metadata areas on this volume") );
+    if( m_pv->getMDAUsed() )
+        m_mda_box->setChecked(true);
+    attrib_box_layout->addWidget(m_mda_box);
+    m_uuid_box = new QCheckBox( i18n("Generate new UUID for this volume") );
+    attrib_box_layout->addWidget(m_uuid_box);
+    enableButtonOk(false);
 
+    connect(m_allocation_box,   SIGNAL(clicked()), this, SLOT(resetOkButton()));
+    connect(m_mda_box,   SIGNAL(clicked()), this, SLOT(resetOkButton()));
+    connect(m_uuid_box,   SIGNAL(clicked()), this, SLOT(resetOkButton()));
+}
+
+void PVChangeDialog::resetOkButton()
+{
+    enableButtonOk(false);
+
+    if( m_allocation_box->isChecked() != m_pv->isAllocatable() )
+        enableButtonOk(true);
+
+    if( m_mda_box->isChecked() != m_pv->getMDAUsed() )
+        enableButtonOk(true);   
+
+    if( m_uuid_box->isChecked() )
+        enableButtonOk(true);
 }
 
 QStringList PVChangeDialog::arguments()
 {
     QStringList args;
 
-    args << "pvchange"
-	 << "--allocatable";
+    args << "pvchange";
+	 
+    if( m_allocation_box->isChecked() && !m_pv->isAllocatable() )
+	args << "--allocatable" << "y";
+    else if( !m_allocation_box->isChecked() && m_pv->isAllocatable() )
+	args << "--allocatable" << "n";
 
-    if(allocation_box->isChecked())
-	args << "y";
-    else
-	args << "n";
+    if( m_mda_box->isChecked() && !m_pv->getMDAUsed() )
+        args << "--metadataignore" << "n";
+    else if( !m_mda_box->isChecked() && m_pv->getMDAUsed() )
+        args << "--metadataignore" << "y";
 
-    args << m_pv_path;
+    if( m_uuid_box->isChecked() )
+        args << "--uuid";
+
+    args << m_pv->getName();
   
     return args;
 }
