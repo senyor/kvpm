d90f67b71be3562cda68154475f4c8d799fb83b7
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2011-12-29 05:21:13 +0000

    Changed lvrename to eliminate the wrapper funcion.

--- trunk/kvpm/lvrename.cpp
+++ trunk/kvpm/lvrename.cpp
@@ -22,69 +22,47 @@
 #include "volgroup.h" 
 #include "processprogress.h"
 
-bool rename_lv(LogVol *logicalVolume)
+
+LVRenameDialog::LVRenameDialog(LogVol *const volume, QWidget *parent) 
+  : KDialog(parent), 
+    m_lv(volume)
 {
-    LVRenameDialog dialog(logicalVolume);
-    dialog.exec();
-    
-    if(dialog.result() == QDialog::Accepted){
-        ProcessProgress rename( dialog.arguments() );
+    setWindowTitle( i18n("Rename Logical Volume") );
 
-        if( ! rename.exitCode() && logicalVolume->isMounted() )
-            MountTables::renameEntries( logicalVolume->getMapperPath(), dialog.getNewMapperPath() );
-
-	return true;
-    }
-    else{
-        return false;
-    }
-}
-
-LVRenameDialog::LVRenameDialog(LogVol *logicalVolume, QWidget *parent) : 
-    KDialog(parent), m_lv(logicalVolume)
-{
-
-    setWindowTitle( i18n("Rename logical volume") );
-
-    QWidget *dialog_body = new QWidget(this);
+    QWidget *const dialog_body = new QWidget(this);
     setMainWidget(dialog_body);
-    QVBoxLayout *layout = new QVBoxLayout();
+    QVBoxLayout *const layout = new QVBoxLayout();
     dialog_body->setLayout(layout);
 
     m_vg_name  = m_lv->getVg()->getName();
     m_old_name = m_lv->getName();
 
-    QLabel *old_name_label = new QLabel( i18n("Old logical volume name: %1", m_old_name) );
-    layout->addWidget(old_name_label);
+    QLabel *label = new QLabel( i18n("<b>Rename Logical Volume</b>") );
+    label->setAlignment(Qt::AlignCenter);
+    layout->addWidget(label);
+    layout->addSpacing(10);
 
-    QLabel *name_label = new QLabel( i18n("New logical volume name: ") );
-    m_new_name = new KLineEdit();
+    label = new QLabel( i18n("Current volume name: %1", m_old_name) );
+    layout->addWidget(label);
 
     QRegExp rx("[0-9a-zA-Z_\\.][-0-9a-zA-Z_\\.]*");
+    m_new_name = new KLineEdit();
     m_name_validator = new QRegExpValidator( rx, m_new_name );
     m_new_name->setValidator(m_name_validator);
-    QHBoxLayout *name_layout = new QHBoxLayout();
-    name_layout->addWidget(name_label);
+    QHBoxLayout *const name_layout = new QHBoxLayout();
+    label = new QLabel( i18n("New volume name: ") );
+
+    name_layout->addWidget(label);
     name_layout->addWidget(m_new_name);
     layout->addLayout(name_layout);
     
     enableButtonOk(false);
 
+    connect(this, SIGNAL(okClicked()), 
+            this, SLOT(commitChanges()));
+
     connect(m_new_name, SIGNAL(textChanged(QString)), 
 	    this, SLOT(validateName(QString)));
-
-}
-
-QStringList LVRenameDialog::arguments()
-{
-    QStringList args;
-    
-    args << "lvrename"
-	 << m_vg_name
-	 << m_old_name
-	 << m_new_name->text();
-
-    return args;
 }
 
 /* The allowed characters in the name are letters, numbers, periods
@@ -112,4 +90,19 @@ QString LVRenameDialog::getNewMapperPath()
     path.truncate( path.lastIndexOf('/') + 1 );
 
     return QString( path + m_new_name->text() );
+}
+
+void LVRenameDialog::commitChanges()
+{
+    QStringList args;
+    
+    args << "lvrename"
+	 << m_vg_name
+	 << m_old_name
+	 << m_new_name->text();
+
+    ProcessProgress rename(args);
+
+    if( !rename.exitCode() && m_lv->isMounted() )
+        MountTables::renameEntries( m_lv->getMapperPath(), getNewMapperPath() );
 }
