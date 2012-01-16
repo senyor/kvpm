43e226376c3ca406b6c87e4342568669c3ab5d3e
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2012-01-16 22:30:02 +0000

    Revamped partadd to remove the wrapper functions. the libparted function to get mac geometry now works correctly so the getMaxSize function was modified to use it correctly. More error conditions are now caught and parts are aligned to 1 MiB at both ends automatically.

--- kvpm/partadd.cpp
+++ kvpm/partadd.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * 
- * Copyright (C) 2009, 2010, 2011 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2009, 2010, 2011, 2012 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the kvpm project.
  *
@@ -31,35 +31,9 @@
    between partitions. It can also be an *empty* extended partition however.
 */
 
-bool add_partition(StoragePartition *partition)
-{
-    PedDisk *disk = partition->getPedPartition()->disk;
 
-    unsigned ped_type = partition->getPedType();
-    bool logical_freespace = ( ped_type & PED_PARTITION_FREESPACE ) && ( ped_type & PED_PARTITION_LOGICAL );
-    int count = ped_disk_get_primary_partition_count(disk);
-    int max_count = ped_disk_get_max_primary_partition_count(disk);
 
-    if( count >= max_count  && ( ! ( logical_freespace || ( ped_type & PED_PARTITION_EXTENDED ) ) ) ){
-        KMessageBox::error(0, i18n("Disk already has %1 primary partitions, the maximum", count));
-        return false;
-    }
-    else if( ( ped_type & PED_PARTITION_EXTENDED ) && ( ! partition->isEmpty() ) ){
-        KMessageBox::error(0, i18n("This should not happen. Try selecting the freespace and not the partiton itself"));
-        return false;
-    }
-    else{
-        PartitionAddDialog dialog(partition);
-        dialog.exec();
-    
-        if(dialog.result() == QDialog::Accepted)
-            return true;
-        else
-            return false;
-    }
-}
-
-PartitionAddDialog::PartitionAddDialog(StoragePartition *partition, QWidget *parent)
+PartitionAddDialog::PartitionAddDialog(StoragePartition *const partition, QWidget *parent)
     : KDialog(parent),
       m_partition(partition)
 {
@@ -67,75 +41,53 @@ PartitionAddDialog::PartitionAddDialog(StoragePartition *partition, QWidget *par
     skeleton.setCurrentGroup("General");
     skeleton.addItemBool("use_si_units", m_use_si_units, false);
 
-    PedPartition *ped_free_partition = m_partition->getPedPartition();
-    PedGeometry   ped_geometry;
-    PedDisk      *ped_disk  = ped_free_partition->disk;
-    PedDisk      *temp_disk = ped_disk_new( ped_disk->dev );
-    PedDevice    *ped_device;
+    if( !(m_bailout = hasInitialErrors()) ){
 
-    // If this is an empty extended partition and not freespace inside
-    // one then look for the freespace.
+        getMaximumPartition(m_max_part_start, m_max_part_end, m_sector_size);
+        m_max_part_size = m_max_part_end - m_max_part_start + 1;
+        
+        if( m_max_part_size >= (0x200000 / m_sector_size) ){   // sectors per megabyte
 
-    if( ped_free_partition->type & PED_PARTITION_EXTENDED ){
-        do{
-            ped_free_partition = ped_disk_next_partition (temp_disk, ped_free_partition);
-            if( ! ped_free_partition )
-                qDebug() << "Extended partition with no freespace found!";
+            setWindowTitle( i18n("Create A New Partition") );
+            
+            QWidget *const dialog_body = new QWidget(this);
+            setMainWidget(dialog_body);
+            QVBoxLayout *const layout = new QVBoxLayout();
+            dialog_body->setLayout(layout);
+            
+            QLabel *const label = new QLabel( i18n("<b>Create A New Partition</b>") );
+            label->setAlignment(Qt::AlignCenter);
+            layout->addSpacing(5);
+            layout->addWidget(label);
+            layout->addSpacing(5);
+            
+            layout->addWidget( buildInfoGroup() );
+            m_dual_selector = new DualSelectorBox(m_sector_size, m_max_part_size);
+            validateChange();
+            
+            layout->addWidget(m_dual_selector);
+            
+            connect(m_dual_selector, SIGNAL(changed()),
+                    this, SLOT(validateChange()));
+            
+            connect(this, SIGNAL(okClicked()),
+                    this, SLOT(commitPartition()));
         }
-        while( !((ped_free_partition->type & PED_PARTITION_FREESPACE) && (ped_free_partition->type & PED_PARTITION_LOGICAL)) );
+        else{
+            m_bailout = true;
+            KMessageBox::error(0, i18n("Not enough usable space for a new partition"));
+        }
     }
-
-    ped_geometry = ped_free_partition->geom;
-    m_ped_disk   = ped_free_partition->disk;
-    ped_device   = m_ped_disk->dev;
-    m_sector_size = ped_device->sector_size;
-
-    /* how big can it grow? */
-    PedGeometry ped_max_geometry = ped_free_partition->geom;
-    m_max_part_start = ped_max_geometry.start;
-    m_max_part_size  = ped_max_geometry.length;
-    m_max_part_end   = m_max_part_start + m_max_part_size - 1; 
-    m_sector_size    = ped_device->sector_size;
-
-    //hacks for msdos label bug in libparted -- disable when fixed!
-    if( QString( m_ped_disk->type->name ) == "msdos" )
-        getMaximumPartition();
-
-    setWindowTitle( i18n("Add Partition") );
-
-    QWidget *dialog_body = new QWidget(this);
-    setMainWidget(dialog_body);
-    QVBoxLayout *layout = new QVBoxLayout();
-    dialog_body->setLayout(layout);
-
-    QLabel *const label = new QLabel( i18n("<b>Create A New Partition</b>") );
-    label->setAlignment(Qt::AlignCenter);
-    layout->addSpacing(5);
-    layout->addWidget(label);
-    layout->addSpacing(5);
- 
-    layout->addWidget( buildInfoGroup() );
-    m_dual_selector = new DualSelectorBox(m_sector_size, m_max_part_size);
-    validateChange();
-
-    layout->addWidget(m_dual_selector);
-
-    connect(m_dual_selector, SIGNAL(changed()),
-            this, SLOT(validateChange()));
-
-    connect(this, SIGNAL(okClicked()),
-	    this, SLOT(commitPartition()));
 }
 
 void PartitionAddDialog::commitPartition()
 {
-    PedDevice *device = m_ped_disk->dev;
+    const PedSector ONE_MIB = 0x100000 / m_sector_size;   // sectors per megabyte
+    PedDevice *const device = m_partition->getPedPartition()->disk->dev;
+    PedDisk   *const disk   = m_partition->getPedPartition()->disk;
+    PedSector first_sector  = m_max_part_start + m_dual_selector->getCurrentOffset();
+    PedSector last_sector   = first_sector + m_dual_selector->getCurrentSize() - 1;
     PedPartitionType type;
-    PedSector first_sector = m_max_part_start + m_dual_selector->getCurrentOffset();
-    PedSector last_sector  = first_sector + m_dual_selector->getCurrentSize() - 1;
-    const PedSector length  = (last_sector - first_sector) + 1;
-
-    const PedSector sectors_1MiB  = 0x100000 / m_sector_size;   // sectors per megabyte
 
     if( first_sector < m_max_part_start )
         first_sector = m_max_part_start;
@@ -143,36 +95,51 @@ void PartitionAddDialog::commitPartition()
     if( last_sector > m_max_part_end )
         last_sector = m_max_part_end;
 
-    if( length < ( 2 * sectors_1MiB ) )
+    if( (1 + last_sector - first_sector) < (2 * ONE_MIB) ){
+        KMessageBox::error(0, i18n("Partitions less than two MiB are not supported"));
         return;
+    }
 
-    PedAlignment *start_align  = ped_alignment_new( 0, sectors_1MiB); 
-    PedAlignment *end_align    = ped_alignment_new(-1, 1);
+    PedAlignment *const start_align = ped_alignment_new( 0, ONE_MIB); 
+    PedAlignment *const end_align   = ped_alignment_new(-1, ONE_MIB);
 
-    PedGeometry *geom_1MiB = ped_geometry_new(device, m_max_part_start, m_max_part_size);
-    first_sector = ped_alignment_align_down(start_align, geom_1MiB, first_sector);
-    last_sector  = first_sector + length - 1;
-    if( last_sector > m_max_part_end )
-        last_sector = m_max_part_end;
+    PedGeometry  *const max_geom = ped_geometry_new(device, m_max_part_start, 1 + m_max_part_end - m_max_part_start);
+    first_sector = ped_alignment_align_nearest(start_align, max_geom, first_sector);
+    last_sector  = ped_alignment_align_nearest(end_align, max_geom, last_sector);
+    ped_geometry_destroy(max_geom);
 
-    PedGeometry *start_range   = ped_geometry_new(device, first_sector, 1 + m_max_part_end - first_sector);
-    PedGeometry *end_range   = ped_geometry_new(device, first_sector, 1 + m_max_part_end - first_sector);
+    const PedSector length = 1 + last_sector - first_sector;
+
+    PedGeometry *const start_range = ped_geometry_new(device, first_sector, length);
+    PedGeometry *const end_range   = ped_geometry_new(device, first_sector, length);
  
-    PedConstraint *constraint = ped_constraint_new( start_align, end_align,
-                                                    start_range, end_range,
-                                                    length - sectors_1MiB, length );
+    PedConstraint *const constraint = ped_constraint_new( start_align, end_align,
+                                                          start_range, end_range,
+                                                          length - ONE_MIB, length );
 
-    if(m_type_combo->currentIndex() == 0)
-        type = PED_PARTITION_NORMAL ;
-    else if(m_type_combo->currentIndex() == 1)
-        type = PED_PARTITION_EXTENDED ;
-    else
-        type = PED_PARTITION_LOGICAL ;
-
-    PedPartition *ped_new_partition = ped_partition_new(m_ped_disk, type, 0, first_sector, last_sector);
-
-    if( ped_disk_add_partition(m_ped_disk, ped_new_partition, constraint) )
-        ped_disk_commit(m_ped_disk);
+    if(constraint != NULL){
+        if(m_type_combo->currentIndex() == 0)
+            type = PED_PARTITION_NORMAL ;
+        else if(m_type_combo->currentIndex() == 1)
+            type = PED_PARTITION_EXTENDED ;
+        else
+            type = PED_PARTITION_LOGICAL ;
+        
+        PedDiskFlag cylinder_flag = ped_disk_flag_get_by_name("cylinder_alignment");
+        if( ped_disk_is_flag_available(disk, cylinder_flag) )
+            ped_disk_set_flag(disk, cylinder_flag, 0);
+        
+        PedPartition *ped_new_partition = ped_partition_new(disk, type, 0, first_sector, last_sector);
+        if(ped_new_partition != NULL){
+            if( ped_disk_add_partition(disk, ped_new_partition, constraint) )
+                ped_disk_commit(disk);
+        }
+        
+        ped_constraint_destroy(constraint);
+    }
+    
+    ped_geometry_destroy(start_range);
+    ped_geometry_destroy(end_range);
 }
 
 long long PartitionAddDialog::convertSizeToSectors(int index, double size)
@@ -198,13 +165,12 @@ long long PartitionAddDialog::convertSizeToSectors(int index, double size)
 
 void PartitionAddDialog::validateChange(){
 
-    const PedSector sectors_1MiB  = 0x100000 / m_sector_size;   // sectors per megabyte
+    const PedSector ONE_MIB  = 0x100000 / m_sector_size;   // sectors per megabyte
     const long long offset = m_dual_selector->getCurrentOffset();
     const long long size   = m_dual_selector->getCurrentSize();
 
-  
     if(( offset <= m_max_part_size - size ) && ( size <= m_max_part_size - offset  ) && 
-       ( size >= 2 * sectors_1MiB ) && ( m_dual_selector->isValid() ) ){
+       ( size >= 2 * ONE_MIB ) && ( m_dual_selector->isValid() ) ){
         enableButtonOk(true);
     }
     else
@@ -248,83 +214,73 @@ void PartitionAddDialog::updatePartition(){
     m_display_graphic->repaint();
 }
 
-void PartitionAddDialog::getMaximumPartition()
+void PartitionAddDialog::getMaximumPartition(PedSector &start, PedSector &end, PedSector &sectorSize)
 {
-    PedPartition *ped_free_part = m_partition->getPedPartition();
+    PedPartition *free_space = m_partition->getPedPartition();
+    PedDevice *const device = free_space->disk->dev;
+    PedDisk   *const disk = ped_disk_new(device);
+    sectorSize = device->sector_size;
+    const PedSector ONE_MIB = 0x100000 / sectorSize;   // sectors per megabyte
 
-    QList<PedPartition *> parts;
-    PedPartition *next_part = NULL;
-    bool is_logical;
-    int index, prev, next;
-    long long max_end = m_max_part_start + m_max_part_size - 1;
-    const PedSector sectors_1MiB  = 0x100000 / m_sector_size;   // sectors per megabyte
+    // If this is an empty extended partition and not freespace inside
+    // one then look for the freespace.
 
-    while( (next_part = ped_disk_next_partition(m_ped_disk, next_part) ) )
-        parts.append(next_part);
-
-    is_logical = ped_free_part->type & PED_PARTITION_LOGICAL;
-
-    // look for the index of the current part
-    for(index = 0; index < parts.size(); index++){
-        if( parts[index]->geom.start == ped_free_part->geom.start )
-            break;
+    if( free_space->type & PED_PARTITION_EXTENDED ){
+        do{
+            free_space = ped_disk_next_partition(disk, free_space);
+            if( !free_space )
+                qDebug() << "Extended partition with no freespace found!";
+        }
+        while( !((free_space->type & PED_PARTITION_FREESPACE) && (free_space->type & PED_PARTITION_LOGICAL)) );
     }
 
-    if( index >= parts.size() ) // We couldn't find the current part?
-        return;
+    start = free_space->geom.start;
+    end = free_space->geom.length + start - 1;
 
-    prev = index - 1;
-    next = index + 1; 
+    PedPartition *part;
 
-    while( prev >= 0 ){
-        if( is_logical == (parts[prev]->type & PED_PARTITION_LOGICAL) ){
-            if( (parts[prev]->type & PED_PARTITION_METADATA) || (parts[prev]->type & PED_PARTITION_FREESPACE) ){
-                if( m_max_part_start > parts[prev]->geom.start ){
-                    m_max_part_start = parts[prev]->geom.start;
-                }
-            }
-            else
-                break;
-        }
-        else{
-            break;
-        }
-        prev--;
-    }
+    if( free_space->type & PED_PARTITION_LOGICAL )
+        part = ped_partition_new(disk, PED_PARTITION_LOGICAL, NULL, start, end);
+    else
+        part = ped_partition_new(disk, PED_PARTITION_NORMAL, NULL, start, end); 
 
-    while( next < parts.size() ){
-        if( is_logical == (parts[next]->type & PED_PARTITION_LOGICAL) ){
-            if( (parts[next]->type & PED_PARTITION_METADATA) || (parts[next]->type & PED_PARTITION_FREESPACE) ){
-                if( max_end < parts[next]->geom.end ){
-                    max_end = parts[next]->geom.end;
-                }
-            }
-            else
-                break;
-        }
-        else{
-            break;
-        }
-        next++;
-    }
+    start = part->geom.start;
+    end = part->geom.length + start - 1;
 
-    if(m_max_part_start < sectors_1MiB)
-        m_max_part_start = sectors_1MiB;
+    PedConstraint *constraint = ped_constraint_any(device);
+    ped_disk_add_partition(disk, part, constraint);
+    ped_constraint_destroy(constraint);
 
-    if(is_logical)
-        m_max_part_start += 64;
+    start = part->geom.start;
+    end = part->geom.length + start - 1;
 
-    m_max_part_end = max_end;
-    m_max_part_size = 1 + m_max_part_end - m_max_part_start;
+    PedDiskFlag cylinder_flag = ped_disk_flag_get_by_name("cylinder_alignment");
+    if( ped_disk_is_flag_available(disk, cylinder_flag) )
+        ped_disk_set_flag(disk, cylinder_flag, 0);
 
-    PedAlignment *align_1MiB = ped_alignment_new(0, sectors_1MiB);
-    PedGeometry *geom_1MiB = ped_geometry_new(ped_free_part->disk->dev, m_max_part_start, m_max_part_size);
-    m_max_part_start = ped_alignment_align_nearest( align_1MiB, geom_1MiB, m_max_part_start);
+    PedGeometry *max_geometry = ped_disk_get_max_partition_geometry(disk, part, constraint);
+    start = max_geometry->start;
+    end = max_geometry->length + max_geometry->start - 1;
 
-    PedAlignment *align_end = ped_alignment_new(-1, 1);
-    m_max_part_end = ped_alignment_align_nearest( align_end, geom_1MiB, m_max_part_end);
+    PedAlignment *const start_align  = ped_alignment_new( 0, ONE_MIB);
+    PedAlignment *const end_align    = ped_alignment_new(-1, ONE_MIB);
+    PedGeometry  *const start_range  = ped_geometry_new(device, start, max_geometry->length);
+    PedGeometry  *const end_range    = ped_geometry_new(device, start, max_geometry->length);
 
-    m_max_part_size = 1 + m_max_part_end - m_max_part_start;
+    constraint = ped_constraint_new(start_align, end_align,
+                                    start_range, end_range,
+                                    1, max_geometry->length);
+
+    ped_geometry_destroy(max_geometry);
+    max_geometry = ped_disk_get_max_partition_geometry(disk, part, constraint);
+    start = max_geometry->start;
+    end = max_geometry->length + max_geometry->start - 1;
+
+    ped_disk_remove_partition(disk, part);
+    ped_partition_destroy(part);
+    ped_constraint_destroy(constraint);
+    ped_geometry_destroy(max_geometry);
+    ped_disk_destroy(disk);
 }
 
 QGroupBox* PartitionAddDialog::buildInfoGroup()
@@ -373,7 +329,7 @@ QGroupBox* PartitionAddDialog::buildInfoGroup()
 KComboBox* PartitionAddDialog::buildTypeCombo()
 {
     KComboBox *const combo = new KComboBox;
-    PedPartition *ped_free_partition = m_partition->getPedPartition();
+    PedPartition *const free_space = m_partition->getPedPartition();
 
     bool logical_freespace;      // true if we are inside an extended partition
     bool extended_allowed;       // true if we can create an extended partition here
@@ -381,23 +337,25 @@ KComboBox* PartitionAddDialog::buildTypeCombo()
     /* check to see if partition table supports extended
        partitions and if it already has one */
 
-    if( (PED_DISK_TYPE_EXTENDED & m_ped_disk->type->features) && 
-	(! ped_disk_extended_partition(m_ped_disk) )  ){
+    PedDisk *const disk  = m_partition->getPedPartition()->disk;
+
+    if( (PED_DISK_TYPE_EXTENDED & disk->type->features) && ( !ped_disk_extended_partition(disk) )  )
         extended_allowed = true;
-    }
     else
         extended_allowed = false;
 
-    if( ped_free_partition->type & PED_PARTITION_LOGICAL )
+    if( free_space->type & PED_PARTITION_LOGICAL )
+        logical_freespace = true;
+    else if( free_space->type & PED_PARTITION_EXTENDED )
         logical_freespace = true;
     else
         logical_freespace = false;
 
-    combo->insertItem(0,"Primary");
-    combo->insertItem(1,"Extended");
+    combo->insertItem(0, i18n("Primary"));
+    combo->insertItem(1, i18n("Extended"));
 
     if(logical_freespace){
-        combo->insertItem(2,"Logical");
+        combo->insertItem(2, i18n("Logical"));
         combo->setEnabled(false);
 	combo->setCurrentIndex(2);
     }
@@ -407,4 +365,29 @@ KComboBox* PartitionAddDialog::buildTypeCombo()
     }
 
     return combo;
+}
+
+bool PartitionAddDialog::hasInitialErrors()
+{
+    PedDisk *const disk = m_partition->getPedPartition()->disk;
+    const unsigned ped_type = m_partition->getPedType();
+    const bool logical_freespace = ( ped_type & PED_PARTITION_FREESPACE ) && ( ped_type & PED_PARTITION_LOGICAL );
+    const int count = ped_disk_get_primary_partition_count(disk);
+    const int max_count = ped_disk_get_max_primary_partition_count(disk);
+
+    if( count >= max_count  && ( !( logical_freespace || ( ped_type & PED_PARTITION_EXTENDED ) ) ) ){
+        KMessageBox::error(0, i18n("This disk already has %1 primary partitions, the maximum", count));
+        return true;
+    }
+    else if( ( ped_type & PED_PARTITION_EXTENDED ) && ( !m_partition->isEmpty() ) ){
+        KMessageBox::error(0, i18n("This should not happen. Try selecting the freespace and not the partiton itself"));
+        return true;
+    }
+
+    return false;
+}
+
+bool PartitionAddDialog::bailout()
+{
+    return m_bailout;
 }
