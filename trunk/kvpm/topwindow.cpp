f087dad8704f115b091258dd0b12449da9828ba9
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2012-03-25 23:05:33 +0000

    Moved credit for Silk icons to KAboutData::addCredit generated tab.

--- trunk/kvpm/topwindow.cpp
+++ trunk/kvpm/topwindow.cpp
@@ -449,18 +449,21 @@ KMenu *TopWindow::buildHelpMenu()
                                                    QByteArray(""),
                                                    ki18n("kvpm"),
                                                    QByteArray("0.8.6"),
-                                                   ki18n("Linux volume and partition manager for KDE.\n"
-                                                         "This program is still under development,\n"
-                                                         "bug reports and any comments are welcomed.\n \n"
-                                                         "Additional icons taken from the Silk icon set by\n"
-                                                         "Mark James http://www.famfamfam.com/lab/icons/silk/\n"
-                                                         "under the Creative Commons Attribution 3.0 License"),
+                                                   ki18n("Linux volume and partition manager for KDE.  "
+                                                         "This program is still under development, "
+                                                         "bug reports and any comments are welcomed.  "
+                                                         "Licensed under GNU General Public License v3.0"),
                                                    KAboutData::License_GPL_V3, 
                                                    ki18n("(c) 2008, 2009, 2010, 2011, 2012 Benjamin Scott"),
-                                                   ki18n(" "),
+                                                   ki18n(""),
                                                    QByteArray("http://sourceforge.net/projects/kvpm/"),
                                                    QByteArray("benscott@nwlink.com") );
 
+    about_data->addCredit( ki18n("Mark James"), 
+                           ki18n("Additional icons taken from the Silk icon set by "
+                                 "Mark James under the Creative Commons Attribution 3.0 License."),
+                           QByteArray(),
+                           QByteArray("http://www.famfamfam.com/lab/icons/silk/") );
 
     KHelpMenu *const help_menu = new KHelpMenu(this, about_data);
 
