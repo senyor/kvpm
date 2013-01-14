ffa7e9fd2d82a16648debb9d94834e41469343a4
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2013-01-14 01:58:20 +0000

    bumped the version to 0.9.5

--- trunk/kvpm/main.cpp
+++ trunk/kvpm/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
 {
 
     KAboutData aboutData("kvpm", 0,
-                         ki18n("kvpm"), "0.9.4",
+                         ki18n("kvpm"), "0.9.5",
                          ki18n("The Linux volume and partition manager for KDE.\n"
                                "Licensed under the GPL v3.0\n \n"
                                "Additional icons taken from the Silk icon set by Mark James.\n"
