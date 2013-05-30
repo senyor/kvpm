ea9e4cb58f664091a5c9d3c43dd09bb181ef2f80
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2013-05-30 23:11:39 +0000

    Made PED exception handling smarter. Exceptions from the PED library that include questions go into YesNo MessageBoxes to be answered and the results passed to libparted.

--- kvpm/pedexceptions.cpp
+++ kvpm/pedexceptions.cpp
@@ -1,7 +1,7 @@
 /*
  *
  *
- * Copyright (C) 2010, 2011, 2012 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2010, 2011, 2012, 2013 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the kvpm project.
  *
@@ -23,21 +23,50 @@
 #include <QString>
 
 
+/*   If the error being handeled has a question we put up a 
+     Yes/No warning box. Otherwise we put up a "sorry dialog" 
+     for warnings and errors or an "error dialog" for bugs, fatal 
+     errors and missing features. Generally a "Yes" choice will 
+     tell Ped to fix the problem while "No" will ignore it.  */
 
 PedExceptionOption my_handler(PedException *exception)
 {
-    QString error_message;
+    QString message(exception->message);
+
+    if (message.contains("unrecognised disk label"))
+        return PED_EXCEPTION_CANCEL;
+
+    qApp->restoreOverrideCursor();                   // reset the cursor to not-busy
+
+    const PedExceptionOption options = exception->options;
 
     if (exception->type == PED_EXCEPTION_INFORMATION){
-        qApp->restoreOverrideCursor();                   // reset the cursor to not-busy
-        KMessageBox::information(0, exception->message);
+        KMessageBox::information(nullptr, message);
+        return PED_EXCEPTION_CANCEL;
+    } else if (exception->type == PED_EXCEPTION_WARNING || exception->type == PED_EXCEPTION_ERROR) {
+        if (options & (PED_EXCEPTION_FIX | PED_EXCEPTION_YES)) {
+            if (KMessageBox::warningYesNo(nullptr, message) == KMessageBox::Yes) {
+                if (options & PED_EXCEPTION_FIX)
+                    return PED_EXCEPTION_FIX;
+                else
+                    return PED_EXCEPTION_YES;
+            } else {
+                if (options & PED_EXCEPTION_IGNORE)
+                    return PED_EXCEPTION_IGNORE;
+                else if (options & PED_EXCEPTION_NO)
+                    return PED_EXCEPTION_NO;
+                else if (options & PED_EXCEPTION_OK)
+                    return PED_EXCEPTION_OK;
+                else
+                    return PED_EXCEPTION_CANCEL;
+            }
+        } else {
+            KMessageBox::sorry(nullptr, message);
+        } 
     } else {
-        error_message = QString(exception->message);
-        if (!error_message.contains("unrecognised disk label")){
-            qApp->restoreOverrideCursor();
-            KMessageBox::error(0, exception->message);
-        }
+        KMessageBox::error(nullptr, message);
+        return PED_EXCEPTION_CANCEL;
     }
 
-    return PED_EXCEPTION_UNHANDLED;
+    return PED_EXCEPTION_CANCEL;
 }
