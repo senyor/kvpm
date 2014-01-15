d72c53570a41300d51c9da3966559eb38afd0756
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2014-01-15 01:09:42 +0000

    Refactored devicesizechart.cpp. Removed unused code and consolidated resizeEvent() with the initial sizing of segments at startup. Converted C casts to static_cast.

--- kvpm/devicesizechart.cpp
+++ kvpm/devicesizechart.cpp
@@ -19,19 +19,19 @@
 #include "storagedevice.h"
 #include "storagepartition.h"
 
+#include <parted/disk.h>
+
 #include <KSeparator>
 
+#include <QDebug>
 #include <QHBoxLayout>
 #include <QResizeEvent>
 #include <QTreeWidget>
 #include <QTreeWidgetItem>
 
 
-DeviceSizeChart::DeviceSizeChart(QWidget *parent) : QFrame(parent)
+DeviceSizeChart::DeviceSizeChart(QWidget *const parent) : QFrame(parent)
 {
-    //    setFrameStyle(QFrame::Sunken | QFrame::Panel);
-    //    setLineWidth(2);
-
     m_layout = new QHBoxLayout();
     m_layout->setSpacing(0);
     m_layout->setMargin(0);
@@ -44,41 +44,63 @@ DeviceSizeChart::DeviceSizeChart(QWidget *parent) : QFrame(parent)
 
 void DeviceSizeChart::setNewDevice(QTreeWidgetItem *deviceItem)
 {
-    QWidget *segment, *extended_segment;
-    QString usage, path;
-    double ratio;
-    QTreeWidgetItem  *partition_item, *extended_item;
-    StoragePartition *partition;
-    StorageDevice    *device;
-    long long part_size, device_size;
-    int max_segment_width;
-    unsigned int part_type;
-
     if (deviceItem == nullptr)
         return;
 
-    const QTreeWidget *const root = deviceItem->treeWidget();
-
     // Find the deviceItem at the top, just under the tree root.
-    while ((deviceItem->parent()) && ((QTreeWidget *)deviceItem->parent() != root))
+    const QTreeWidget *const root = deviceItem->treeWidget();
+    while ((deviceItem->parent()) && (reinterpret_cast<QTreeWidget *>(deviceItem->parent()) != root))
         deviceItem = deviceItem->parent();
 
-    for (int x = m_layout->count() - 1; x >= 0; x--) // delete all the children
-        m_layout->takeAt(x)->widget()->deleteLater();
+    for (int i = m_layout->count() - 1; i >= 0; --i) // delete all the children
+        m_layout->takeAt(i)->widget()->deleteLater();
 
     m_segments.clear();
     m_ratios.clear();
     m_extended_segments.clear();
     m_extended_ratios.clear();
-    device = (StorageDevice *)((deviceItem->data(1, Qt::UserRole).value<void *>()));
 
-    if (!deviceItem->childCount()) {
+    const StorageDevice *device = static_cast<StorageDevice *>((deviceItem->data(1, Qt::UserRole).value<void *>()));
+    QWidget *segment = nullptr;
 
-        if (!device->isPhysicalVolume())
-            usage = "physical volume";
-        else
-            usage = "";
-
+    if (deviceItem->childCount()) {   // device with one or more partitions
+        for (int i = 0; i < deviceItem->childCount(); ++i) {
+            
+            QTreeWidgetItem *const part_item = deviceItem->child(i);
+            const StoragePartition *part = static_cast<StoragePartition *>((part_item->data(0, Qt::UserRole)).value<void *>());
+            double ratio = part->getSize() / static_cast<double>(device->getSize());
+            
+            segment = new DeviceChartSeg(part_item);
+            m_segments.append(segment);
+            m_ratios.append(ratio);
+            
+            connect(segment, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)),
+                    this, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)));
+            
+            if (part->getPedType() & PED_PARTITION_EXTENDED) {
+                m_extended_layout = new QHBoxLayout();
+                m_extended_layout->setSpacing(0);
+                m_extended_layout->setMargin(0);
+                m_extended_layout->setSizeConstraint(QLayout::SetNoConstraint);
+                if (!part->isEmptyExtended()) {
+                    for (int j = 0 ; j < part_item->childCount(); ++j) {
+                        QTreeWidgetItem  *const extended_item = part_item->child(j);
+                        part = static_cast<StoragePartition *>((extended_item->data(0, Qt::UserRole)).value<void *>());
+                        QWidget *const extended_segment = new DeviceChartSeg(extended_item);
+                        ratio = part->getSize() / static_cast<double>(device->getSize());
+                        m_extended_segments.append(extended_segment);
+                        m_extended_ratios.append(ratio);
+                        m_extended_layout->addWidget(extended_segment);
+                        
+                        connect(extended_segment, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)),
+                                this, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)));
+                    }
+                }
+                segment->setLayout(m_extended_layout);
+            }
+            m_layout->addWidget(segment);
+        }
+    } else {
         segment = new DeviceChartSeg(deviceItem);
         m_segments.append(segment);
         m_ratios.append(1.0);
@@ -86,103 +108,35 @@ void DeviceSizeChart::setNewDevice(QTreeWidgetItem *deviceItem)
 
         connect(segment, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)),
                 this, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)));
-
     }
 
-    for (int x = 0; x < deviceItem->childCount(); x++) {
-        partition_item = deviceItem->child(x);
+    resizeSegments(width());
+}
 
-        partition = (StoragePartition *)((partition_item->data(0, Qt::UserRole)).value<void *>());
+void DeviceSizeChart::resizeSegments(const int width)
+{
+    int max_width;
+    
+    for (int i = m_segments.size() - 1 ; i >= 0; --i) {
+        
+        max_width = ((width * m_ratios[i]) - 2);
+        if (max_width < 1)
+            max_width = 1;
 
-        part_type = partition->getPedType();
-        part_size = partition->getSize();
-        device_size = device->getSize();
-
-        if (partition->isPhysicalVolume())
-            usage = "physical volume";
-        else
-            usage = (partition_item->data(3, Qt::DisplayRole)).toString();
-
-        ratio = part_size / (double) device_size;
-        segment = new DeviceChartSeg(partition_item);
-        m_segments.append(segment);
-        m_ratios.append(ratio);
-
-        connect(segment, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)),
-                this, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)));
-
-        if (part_type & 0x02) {  // extended partition
-            m_extended_layout = new QHBoxLayout();
-            m_extended_layout->setSpacing(0);
-            m_extended_layout->setMargin(0);
-            m_extended_layout->setSizeConstraint(QLayout::SetNoConstraint);
-            if (!partition->isEmptyExtended()) {
-                for (int y = 0 ; y < partition_item->childCount(); y++) {
-                    extended_item = partition_item->child(y);
-                    partition = (StoragePartition *)((extended_item->data(0, Qt::UserRole)).value<void *>());
-                    device = (StorageDevice *)((extended_item->data(1, Qt::UserRole)).value<void *>());
-                    part_type = partition->getPedType();
-                    part_size = partition->getSize();
-                    device_size = device->getSize();
-                    if (partition->isPhysicalVolume())
-                        usage = "physical volume";
-                    else
-                        usage = (extended_item->data(3, Qt::DisplayRole)).toString();
-                    extended_segment = new DeviceChartSeg(extended_item);
-                    ratio = part_size / (double) device_size;
-                    m_extended_segments.append(extended_segment);
-                    m_extended_ratios.append(ratio);
-
-                    m_extended_layout->addWidget(extended_segment);
-
-                    connect(extended_segment, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)),
-                            this, SIGNAL(deviceMenuRequested(QTreeWidgetItem *)));
-                }
-            }
-            segment->setLayout(m_extended_layout);
-        }
-        m_layout->addWidget(segment);
+        m_segments[i]->setMaximumWidth(max_width);
     }
 
-    for (int x = m_segments.size() - 1 ; x >= 0; x--) {
+    for (int i = m_extended_segments.size() - 1; i >= 0; --i) {
 
-        max_segment_width = (int)((width() * m_ratios[x]) - 2);
-        if (max_segment_width < 1)
-            max_segment_width = 1;
+        max_width = ((width * m_extended_ratios[i]) - 2);
+        if (max_width < 1)
+            max_width = 1;
 
-        m_segments[x]->setMaximumWidth(max_segment_width);
-    }
-
-    for (int x = m_extended_segments.size() - 1; x >= 0; x--) {
-
-        max_segment_width = (int)((width() * m_extended_ratios[x]) - 2);
-        if (max_segment_width < 1)
-            max_segment_width = 1;
-
-        m_extended_segments[x]->setMaximumWidth(max_segment_width);
+        m_extended_segments[i]->setMaximumWidth(max_width);
     }
 }
 
-void DeviceSizeChart::resizeEvent(QResizeEvent *event)
+void DeviceSizeChart::resizeEvent(QResizeEvent *const event)
 {
-    int max_segment_width;
-    int new_width = (event->size()).width();
-
-    for (int x = m_segments.size() - 1 ; x >= 0; x--) {
-
-        max_segment_width = (int)((new_width * m_ratios[x]) - 2);
-        if (max_segment_width < 1)
-            max_segment_width = 1;
-
-        m_segments[x]->setMaximumWidth(max_segment_width);
-    }
-
-    for (int x = m_extended_segments.size() - 1; x >= 0; x--) {
-
-        max_segment_width = (int)((new_width  * m_extended_ratios[x]) - 2);
-        if (max_segment_width < 1)
-            max_segment_width = 1;
-
-        m_extended_segments[x]->setMaximumWidth(max_segment_width);
-    }
+    resizeSegments(event->size().width());
 }
