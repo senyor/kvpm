33697137524517dab71384bc708c006313ac93c3
benscott <benscott@2ff30a64-0bd0-434d-9a8d-60c33709ac39>
2016-08-18 18:56:53 +0000

    More kde framework updates and deprecated class removal.

--- trunk/kvpm/pvactions.h
+++ trunk/kvpm/pvactions.h
@@ -1,7 +1,7 @@
 /*
  *
  *
- * Copyright (C) 2013 Benjamin Scott   <benscott@nwlink.com>
+ * Copyright (C) 2013, 2016 Benjamin Scott   <benscott@nwlink.com>
  *
  * This file is part of the Kvpm project.
  *
@@ -17,8 +17,6 @@
 
 
 #include <KActionCollection>
-
-class KDialog;
 
 class QTreeWidgetItem;
 class QAction;
